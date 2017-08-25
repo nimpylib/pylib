94d738b0dc2363677a810d9f7d144c854e0036f2
Daniil Yarancev <21169548+Yardanico@users.noreply.github.com>
2017-08-25 11:03:24 +0300

    Additions and fixes

---
+++ example.nim
@@ -0,0 +1,32 @@
+# Almost direct translation of https://github.com/famzah/langs-performance/blob/master/primes.py
+import pylib
+import times
+import os
+import math
+
+proc get_primes7(n: int): seq[int] = 
+  if n < 2:
+    return @[]
+  if n == 2:
+    return @[2]
+  var s = range(3, n + 1, 2)
+  var mroot = n ** 0.5
+  var half = len(s)
+  var i = 0
+  var m = 3
+  while m <= mroot:
+    if s[i]:
+      var j = (m * m - 3) // 2
+      s[j] = 0
+      while j < half:
+        s[j] = 0
+        j += m
+    i = i + 1
+    m = 2 * i + 3
+  result = @[2]
+  for x in s:
+    if x:
+      result.add(x)
+
+let res = get_primes7(10000000)
+print("Found $1 prime numbers.".format(len(res)))
--- pylib.nim
+++ pylib.nim
@@ -1,5 +1,6 @@
 import strutils, math, sequtils, strfmt, macros, unicode, tables
 export strfmt, math, tables, strutils
+
 type 
   Iterable[T] = concept x
     for value in x:
@@ -12,7 +13,19 @@ const
 template `*`*(a: string | char, b: int): string = a.repeat(b)
 template str*(a: untyped): string = $a
 template int*(a: string): BiggestInt = parseBiggestInt(a)
+template int*[T: SomeNumber](a: T): untyped = system.int(a)
 template float*(a: string): BiggestFloat = parseFloat(a)
+template float*[T: SomeNumber](a: T): untyped = system.float(a)
+template `**`*[T](a: T, b: Natural): T = a ^ b
+template `**`*[T: SomeReal](a, b: T): T = pow(a, b)
+template `**`*[A: SomeReal, B: SomeInteger](a: A, b: B): A = pow(a, b) 
+template `**`*[A: SomeInteger; B: SomeReal](a: A, b: B): B = pow(B(a), b)
+template `<=`*[A: SomeInteger, B: SomeReal](a: A, b: B): bool = B(a) < b
+
+proc `//`*[A, B: SomeNumber](a: A, b: B): float | int = 
+  let data = float(trunc(a / b))
+  if data.trunc == data:
+    result = int(data)
 
 converter bool*[T](arg: T): bool = 
   # If we have len proc for this object
@@ -59,9 +72,13 @@ proc input*(prompt = ""): string =
     stdout.write(prompt)
   stdin.readLine()
 
-iterator range*[T](start, stop: T, step: int): T = 
+iterator range*[T](start, stop: T, step: int, dummy = false): T = 
   ## Range iterator similar to python's "range"
   ## Supports negative values!
+  # dummy is used to distinguish iterators from templates, so
+  # templates wouldn't end in an endless recursion
+  if step == 0:
+    raise newException(ValueError, "Step must not be zero!")
   if step > 0 and stop > 0:
     for x in countup(start, stop - 1, step):
       yield x
@@ -69,16 +86,22 @@ iterator range*[T](start, stop: T, step: int): T =
     for x in countdown(start, stop + 1, -step):
       yield x
 
-iterator range*[T](start, stop: T): T = 
-  for x in range(start, stop, 1): 
+iterator range*[T](start, stop: T, dummy = false): T =
+  ## Range iterator similar to python's "range"
+  ## Supports negative values!
+  for x in start..<stop: 
     yield x
 
-iterator range*[T](stop: T): T = 
-  for x in range(T(0), stop, 1):
+iterator range*[T](stop: T, dummy = false): T = 
+  ## Range iterator similar to python's "range"
+  ## Supports negative values!
+  for x in T(0)..<stop:
     yield x
 
-template range*[T](start, stop: T): seq[T] = toSeq(range(start, stop, step = 1))
-template range*[T](stop: T): seq[T] = toSeq(range(0, stop, step = 1))
+# Templates for range so you don't need to use toSeq manually
+template range*[T](start, stop: T, step: int): seq[T] = toSeq(range(start, stop, step, true))
+template range*[T](start, stop: T): seq[T] = toSeq(range(start, stop, true))
+template range*[T](stop: T): seq[T] = toSeq(range(stop, true))
 
 proc all[T](iter: Iterable[T]): bool = 
   for element in iter:
@@ -88,6 +111,32 @@ proc all[T](iter: Iterable[T]): bool =
 
 proc any[T](iter: Iterable[T]): bool = 
   for element in iter:
-    if element:
+    if bool(element):
       return True
   return False
+
+
+when isMainModule:
+  import unittest
+  suite "Python functions in Nim":
+    test "Range-like Nim procedure":
+      checkpoint "One argument - stop"
+      check range(0).len == 0
+      check range(5) == @[0, 1, 2, 3, 4]
+      checkpoint "Two arguments - start and stop"
+      check range(3, 5) == @[3, 4]
+      checkpoint "Negative start and positive stop"
+      check range(-7, 3) == @[-7, -6, -5, -4, -3, -2, -1, 0, 1, 2]
+      checkpoint "3 positive arguments"
+      check range(1, 10, 3) == @[1, 4, 7]
+      checkpoint "Positive start, negative stop and step"
+      check range(0, -10, -2) == @[0, -2, -4, -6, -8]
+      check range(5, -5, -3) == @[5, 2, -1, -4]
+      checkpoint "Variables"
+      let a = 10
+      let b = 100
+      let c = 50
+      check range(a, a+2) == @[a, a + 1]
+      checkpoint "Errors"
+      expect ValueError:
+        let data = range(1, 2, 0)
