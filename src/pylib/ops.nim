b367bd1134d5b7a62f5ccc0f0288a16c6bdea178
juancarlospaco <juancarlospaco@gmail.com>
2018-05-05 14:16:25 -0300

    SomeReal is Deprecated, use SomeFloat.

--- src/pylib/ops.nim
+++ src/pylib/ops.nim
@@ -2,38 +2,38 @@ import math
 
 # Power templates for different types of arguments
 template `**`*[T](a: T, b: Natural): T = a ^ b
-template `**`*[T: SomeReal](a, b: T): T = pow(a, b)
-template `**`*[A: SomeReal, B: SomeInteger](a: A, b: B): A = pow(a, b) 
-template `**`*[A: SomeInteger; B: SomeReal](a: A, b: B): B = pow(B(a), b)
+template `**`*[T: SomeFloat](a, b: T): T = pow(a, b)
+template `**`*[A: SomeFloat, B: SomeInteger](a: A, b: B): A = pow(a, b)
+template `**`*[A: SomeInteger; B: SomeFloat](a: A, b: B): B = pow(B(a), b)
 
 # Comparasion operators. We only need 3 of them :<, <=, ==.
 # Other comparasion operators are just shortcuts to these
-template `<`*[A: SomeInteger, B: SomeReal](a: A, b: B): bool = B(a) < b
-template `<`*[A: SomeReal, B: SomeInteger](a: A, b: B): bool = a < A(b)
+template `<`*[A: SomeInteger, B: SomeFloat](a: A, b: B): bool = B(a) < b
+template `<`*[A: SomeFloat, B: SomeInteger](a: A, b: B): bool = a < A(b)
 
-template `<=`*[A: SomeInteger, B: SomeReal](a: A, b: B): bool = B(a) <= b
-template `<=`*[A: SomeReal, B: SomeInteger](a: A, b: B): bool = a <= A(b)
+template `<=`*[A: SomeInteger, B: SomeFloat](a: A, b: B): bool = B(a) <= b
+template `<=`*[A: SomeFloat, B: SomeInteger](a: A, b: B): bool = a <= A(b)
 
-template `==`*[A: SomeInteger, B: SomeReal](a: A, b: B): bool = B(a) == b
-template `==`*[A: SomeReal, B: SomeInteger](a: A, b: B): bool = a == A(b)
+template `==`*[A: SomeInteger, B: SomeFloat](a: A, b: B): bool = B(a) == b
+template `==`*[A: SomeFloat, B: SomeInteger](a: A, b: B): bool = a == A(b)
 
 template `/`*(x, y: BiggestInt): float = system.`/`(int(x), int(y))
 
-proc `//`*[A, B: SomeReal | SomeInteger](a: A, b: B): int | float = 
+proc `//`*[A, B: SomeFloat | SomeInteger](a: A, b: B): int | float =
   ## Python-like floor division
   let data = floor(float(a) / float(b))
   # Both arguments are float - result if float
-  when A is SomeReal and B is SomeReal: result = data
+  when A is SomeFloat and B is SomeFloat: result = data
   # Convert result to float
-  elif A is SomeReal: result = A(data)
+  elif A is SomeFloat: result = A(data)
   # Convert result to float
-  elif B is SomeReal: result = B(data)
+  elif B is SomeFloat: result = B(data)
   # Both arguments are int - result is int
   else: result = int(data)
 
-proc `%`*[T: SomeNumber](a, b: T): T = 
+proc `%`*[T: SomeNumber](a, b: T): T =
   ## Python-like modulo
   result = a - b * (a // b)
 
-template `%`*[A: SomeReal, B: SomeInteger](a: A, b: B): A = a % A(b)
-template `%`*[A: SomeInteger; B: SomeReal](a: A, b: B): B = B(a) % b
+template `%`*[A: SomeFloat, B: SomeInteger](a: A, b: B): A = a % A(b)
+template `%`*[A: SomeInteger; B: SomeFloat](a: A, b: B): B = B(a) % b
