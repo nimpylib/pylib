293c9e19a0f2b516e56819895e6a6c142bc45db6
juancarlospaco <juancarlospaco@gmail.com>
2018-05-04 21:02:32 -0300

    Add mimic function for Pythons divmod.

--- src/pylib.nim
+++ src/pylib.nim
@@ -5,16 +5,16 @@ import pylib/[
   pyrandom]
 export class, print, types, ops, strops, pystring, tonim, pyrandom
 
-type 
+type
   Iterable*[T] = concept x
     for value in x:
       value is T
-  
+
 const
   True* = true
   False* = false
 
-converter bool*[T](arg: T): bool = 
+converter bool*[T](arg: T): bool =
   ## Converts argument to boolean
   ## checking python-like truthiness
   # If we have len proc for this object
@@ -32,22 +32,29 @@ converter bool*[T](arg: T): bool =
 
 converter toStr[T](arg: T): string = $arg
 
-proc input*(prompt = ""): string = 
+proc input*(prompt = ""): string =
   ## Python-like input procedure
   if prompt.len > 0:
     stdout.write(prompt)
   stdin.readLine()
 
-proc all*[T](iter: Iterable[T]): bool = 
+proc all*[T](iter: Iterable[T]): bool =
   ## Checks if all values in iterable are truthy
   result = true
   for element in iter:
     if not bool(element):
       return false
 
-proc any*[T](iter: Iterable[T]): bool = 
+proc any*[T](iter: Iterable[T]): bool =
   ## Checks if at least one value in iterable is truthy
   result = false
   for element in iter:
     if bool(element):
       return true
+
+# Mimics Pythons divmod().
+proc divmod*(a: int, b: int):     array[0..1, int]   = [int(a / b), int(a mod b)]
+proc divmod*(a: int8, b: int8):   array[0..1, int8]  = [int8(a / b), int8(a mod b)]
+proc divmod*(a: int16, b: int16): array[0..1, int16] = [int16(a / b), int16(a mod b)]
+proc divmod*(a: int32, b: int32): array[0..1, int32] = [int32(a / b), int32(a mod b)]
+proc divmod*(a: int64, b: int64): array[0..1, int64] = [int64(a / b), int64(a mod b)]
