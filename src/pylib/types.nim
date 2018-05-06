b326fdc47f4c0be38fe8b24533dd96ecd8e6df66
juancarlospaco <juancarlospaco@gmail.com>
2018-05-06 07:24:56 -0300

    Allow int('9'), int from char type.

--- src/pylib/types.nim
+++ src/pylib/types.nim
@@ -2,6 +2,7 @@ import strutils
 
 template str*(a: untyped): string = $a
 template int*(a: string): BiggestInt = parseBiggestInt(a)
+template int*(a: char): BiggestInt = parseBiggestInt($a)
 template int*[T: SomeNumber](a: T): untyped = system.int(a)
 template float*(a: string): BiggestFloat = parseFloat(a)
 template float*[T: SomeNumber](a: T): untyped = system.float(a)
