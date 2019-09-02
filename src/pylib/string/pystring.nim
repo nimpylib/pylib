69799970a535648a371c4cbd64242db40f7dfa4a
Juan Carlos <juancarlospaco@gmail.com>
2019-09-01 22:33:43 -0300

    Use func when possible, clean out whitespaces

--- src/pylib/string/pystring.nim
+++ src/pylib/string/pystring.nim
@@ -11,17 +11,17 @@ const
   whitespace* = " \t\n\r\x0b\x0c"
   printable* = digits & ascii_letters & punctuation & whitespace
 
-proc index*[T: string | char](a: string, b: T, start = 0, last = 0): int =
+func index*[T: string | char](a: string, b: T, start = 0, last = 0): int =
   result = a.find(b, start, last)
   if result == -1:
     raise newException(ValueError, "substring not found")
 
-proc rindex*[T: string | char](a: string, b: T, start = 0, last = 0): int =
+func rindex*[T: string | char](a: string, b: T, start = 0, last = 0): int =
   result = a.rfind(b, start, last)
   if result == -1:
     raise newException(ValueError, "substring not found")
 
-proc isspace*[T: string | char](a: T): bool = unicode.isSpace($a)
+func isspace*[T: string | char](a: T): bool = unicode.isSpace($a)
 
 template join*(sep: string, a: openArray[untyped]): string =
   ## Mimics Python join() -> string
@@ -31,33 +31,31 @@ template join*(sep: char, a: openArray[untyped]): string =
   ## Mimics Python join() -> string
   a.join($sep)
 
-proc casefold*[T: string | char](a: T): string | char =
+func casefold*[T: string | char](a: T): string | char =
   ## Mimics Python str.casefold() -> bool
   unicode.toLower(a)
 
-proc center*[T: string | char](a: T, width: Natural, fillchar: char=' '): string | char =
+func center*[T: string | char](a: T, width: Natural, fillchar: char=' '): string | char =
   ## Mimics Python str.center(width: int, fillchar: str=" ") -> str
   repeat(fillchar, width) & a & repeat(fillchar, width)
 
-proc capwords*[T: string](a: T, sep: char=' '): string =
+func capwords*[T: string](a: T, sep: char=' '): string =
   ## Mimics Python string.capwords(sep) -> str
-  for word in split($a.strip(), $sep):
+  for word in split(strutils.strip($a), $sep):
       result.add(strutils.capitalizeAscii(word))
       result.add($sep)
-  result.strip()
+  result = strutils.strip(result)
 
-proc isalnum*[T: string | char](a: T): bool =
+func isalnum*[T: string | char](a: T): bool =
   ## Mimics Python str.isalnum() -> bool
   strutils.isAlphaNumeric($a)
 
-
-proc `or`*(a, b: string): string =
+func `or`*(a, b: string): string =
   ## Mimics Python str or str -> str.
   ## "or" for string,return a if a is not "" or empty else b,or empty if b is "".
   if a != "": a else: b
 
-
-proc `not`*(s: string): bool =
+func `not`*(s: string): bool =
   ## # Mimics Python not str -> bool.
   ## "not" for strings, return true if the string is not nil or empty.
   s == ""
