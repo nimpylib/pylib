da5a2ee3c86179118934cbe0865883a846c72c1a
Danil Yarantsev <tiberiumk12@gmail.com>
2020-08-03 19:50:19 +0300

    Forgot to commit

--- src/pylib/enumerate.nim
+++ src/pylib/enumerate.nim
@@ -1,5 +1,7 @@
-import macros
+import macros, strformat
 
+# It's not actually needed here, but I'll just keep it
+# so you can understand which experimental feature we're using
 {.experimental: "forLoopMacros".}
 
 macro enumerate*(iterable: ForLoopStmt): untyped =
@@ -16,17 +18,26 @@ macro enumerate*(iterable: ForLoopStmt): untyped =
     var a, b: int
     for i, x in enumerate([0, 1 , 2, 3]): inc a, i; inc b, x
     doAssert a == b
+  expectLen iterable, 4
+  let call = iterable[2]
+  let body = iterable[^1]
 
-  expectKind iterable, nnkForStmt
+  var startingValue: NimNode
+  if call.len < 2:
+    error("enumerate() missing required argument 'iterable'", call)
+  if call.len == 2:
+    startingValue = newLit(0)
+  elif call.len == 3:
+    startingValue = call[2]
+  else:
+    error(fmt"enumerate() takes at most 2 arguments ({call.len - 1} given)", call)
+
   result = newStmtList()
-  var body = iterable[^1]
-  if body.kind != nnkStmtList: body = newTree(nnkStmtList, body)
   var newFor = newTree(nnkForStmt)
-  result.add newVarStmt(iterable[0], 0.newLit)
+  result.add newVarStmt(iterable[0], startingValue)
   body.insert(body.len, newCall(bindSym"inc", iterable[0]))
   for i in 1 .. iterable.len - 3: newFor.add iterable[i]
   newFor.add iterable[^2][1]
   newFor.add body
   result.add newFor
-  # result.add newCall(bindSym"reset", iterable[0])
   result = newBlockStmt(result)
