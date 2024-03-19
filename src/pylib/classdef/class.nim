4effc7fea68c07a48fc72031bf32b2159737da44
litlighilit <litlighilit@foxmail.com>
2024-03-19 15:02:36 +0800

    support `super(...).f(...)` in `class`

--- src/pylib/classdef/class.nim
+++ src/pylib/classdef/class.nim
@@ -23,9 +23,99 @@ proc parseDeclWithType(def: NimNode): tuple[name, typ, val: NimNode] =
   result.typ = typ
   result.val = defVal
 
+#proc mapSuper(nCall: NimNode): NimNode = discard
+
+proc replaceSuperCall(n: var NimNode, defSupCls: NimNode): bool =
+  template nret(cond): untyped =
+    if not cond:
+      return
+  nret n.kind == nnkCall
+  nret n[0].kind == nnkDotExpr
+  let callSup = n[0][0]
+  nret callSup.kind == nnkCall
+  nret callSup[0].eqIdent "super"
+
+  var supCls = defSupCls
+  let cLen = callSup.len
+  if cLen > 1:
+    expectIdent callSup[1], "self"
+  if callSup.len > 2:
+    if cLen > 3: error "super(self, <SupCls>) expected, but got too many args"
+    supCls = callSup[2]
+
+  n[0][0] = newCall(supCls, ident"self")
+
+  #let meth = n[0][1]
+  #let args = n[1..^1]
+
+  n = newCall(ident"procCall", n)
+  result = true
+
+proc recReplaceSuperCall*(n: NimNode, defSupCls: NimNode, start=0): NimNode =
+  ##[ Recursively maps `super(...).f(...)`
+    to `procCall(<SuperClass>(self).f(...))`
+
+The AST map:
+```
+  Call
+    DotExpr
+      Call
+        Ident "super"
+        [Ident "self"]
+        [<SuperClass>]
+      Ident "f"
+      <args>
+
+      |
+      |
+      v
+  
+  Command
+    Ident "procCall"
+    Call
+      DotExpr
+        Call
+          <SuperClass>
+          Ident "self"
+        Ident "f"
+```  ]##
+  runnableExamples:
+    import std/[macros, strutils]
+    macro checkSupSub(resStr: static string; b) =
+      let res = recReplaceSuperCall(b, ident"SupCls")
+      assert resStr == repr(res).strip(), repr(res)
+      result = newStmtList()
+    checkSupSub("procCall(SupCls(self).f())"):
+      super().f()
+    checkSupSub("procCall(SS(self).f(a, b))"):
+      super(self, SS).f(a, b)
+    checkSupSub("a = procCall(SupCls(self).f(a, b))"):
+      a = super().f(a, b)
+    checkSupSub("a = procCall(SupCls(self).f(procCall(SupCls(self).m()), b))"):
+      a = super().f(super().m(), b)
+    checkSupSub("echo(1)"): echo(1)
+
+    # NOTE: the following is not supported, so left unchanged
+    checkSupSub("super()"): super()
+    checkSupSub("super().a"): super().a
+
+  result = n.copy()
+  var i = start
+  while i<result.len:
+    discard replaceSuperCall(result, defSupCls)
+    if result[i].len != 0:
+      result[i] = recReplaceSuperCall(result[i], defSupCls, 0)
+    i.inc
+
 macro class*(obj, body: untyped): untyped = 
-  ##[
-*NOTE*: Now the implement assume each `def` in each child class
+  ##[ minic Python's `class`.
+
+support `def` for method
+and `super(...).method`
+
+## *NOTE*:
+### Method Overwrite 
+Now the implement assume each `def` in each child class
 overwrite parent class's `def`, which is surely not always true,
 
 What's more, it will be false in some less-noted cases:
@@ -40,6 +130,18 @@ The above code will cause `Warning: use {.base.} for base methods; baseless meth
 as the rettype of previous one is int, while the latter is float,
 thus no override and no dynamic dispatch is performed.
 
+### `super` function
+Now support `super([self[,SubCls]]).f([arg,...])`
+
+However, only support the one usage above.
+
+That's, neither `a=super()` nor `super().a` is supported
+
+For the precious one, as now `super` is implemented via Nim's `procCall`,
+it's not easy to bind `super()`(aka. a `SupCls` instance) to a variable.
+
+For the latter one, as Nim doesn't allow override `SupCls`'s attr (lead to a compile-error),
+so if wantting the attr inherited from SupCls, just write it as-is (e.g. `self.a`)
 ]##
   runnableExamples:
     class O:
@@ -61,18 +163,26 @@ thus no override and no dynamic dispatch is performed.
     class C:
       def f(self, b: float) -> int: return 1+int(b)
     assert C().f(2) == 3
+
+    class CC(C):
+      def f(self, b: float):
+        return super().f(b)
+    assert CC().f(2) == 3
   
   # We accept "class Shape:" "class Shape():" or  "Class Shape(object):"
   var
     classId = obj
-    supClsNode = nnkOfInherit.newTree ident"RootObj"
+    supCls = ident"RootObj"
+    supClsNode = nnkOfInherit.newTree supCls
     pragmas = nnkPragma.newTree ident"base"
+    
   if obj.kind != nnkIdent:  #  class O([SupCls])
     classId = obj[0]
     expectKind obj, nnkCall
     let supLen = obj.len - 1
     if supLen == 1:   #  class O(SupCls)
-      supClsNode = nnkOfInherit.newTree obj[1]
+      supCls = obj[1]
+      supClsNode = nnkOfInherit.newTree supCls
       pragmas = emptyn
     elif supLen > 1:
       error "multi-inhert is not allowed in Nim, " &
@@ -116,7 +226,7 @@ thus no override and no dynamic dispatch is performed.
           new(self)
         beforeBody.add getAst(construct())
       # Function body
-      var parsedbody = parseBody def[2]
+      var parsedbody = recReplaceSuperCall(parseBody def[2], supCls)
       # If we're generating a constructor proc - we need to return self
       # after we've created it
       if isConstructor:
