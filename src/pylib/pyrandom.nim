da21c6142ff763603997d66c4fa2bd7ace4aceba
litlighilit <litlighilit@foxmail.com>
2024-03-25 19:25:18 +0800

    cleanup libs in old places

--- src/pylib/pyrandom.nim
+++
@@ -1,7 +0,0 @@
-import std/random
-export random
-
-template seed*() = randomize()
-template seed*(val: SomeNumber) = randomize(int(val))
-template choice*[T](data: openArray[T]): T = sample(data)
-template randint*[T: SomeInteger](a, b: T): int = rand(a .. b)
