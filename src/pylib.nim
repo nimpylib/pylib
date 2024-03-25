9a5a564070ff3fef6931b8c26cf26db60b5c6371
litlighilit <litlighilit@foxmail.com>
2024-03-25 14:00:01 +0800

    `input` support nodejs (ASCII-only)

--- src/pylib.nim
+++ src/pylib.nim
@@ -272,11 +272,31 @@ proc input*(prompt = ""): string =
     when not defined(nodejs):  # browesr or deno
       asm "`jsResStr` = prompt(`jsPs`);"
     else:
-      {.error: "not impl for NodeJs".}
-      asm """
-    ;{
+      asm """ // XXX: only support ASCII charset now.
+        if (`jsPs`) process.stdout.write(`jsPs`);
+        const fs = require('fs');
+        let fd = (process.platform == 'win32') ?
+          process.stdin.fd :
+          fs.openSync('/dev/tty', 'rs');
+        let buf = Buffer.alloc(4);
+        let str = '', read;
+        while (true) {
+          read = fs.readSync(fd, buf, 0, 1);
+          if(read==0) continue;
+          let chr = buf[0];
+          
+          // catch the newline character
+          if (chr == 10) {
+            fs.closeSync(fd);
+            break;
+          }
+          str += buf.slice(0,1).toString();
+        }
+        `jsResStr` = str;
+      """
+      #[
+      asm """ ;{
     let rlmod = require("readline")
-    let jsArr = new Array();
     let rlinter = rlmod.createInterface(
       {input: process.stdin, output: process.stdout});
     rlinter.question(`jsPs`, inp=>{
@@ -285,7 +305,7 @@ proc input*(prompt = ""): string =
       // So `result` will be just null when returned
     });
     }
-"""
+    """]#
     result = $jsResStr
   else:
     if prompt.len > 0:
