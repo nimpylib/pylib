09a4386a6347d585807f6e63ea17a0a72cfcc503
litlighilit <litlighilit@foxmail.com>
2024-04-06 22:36:51 +0800

    adjust `%` `//` as Py's

--- src/pylib/ops.nim
+++ src/pylib/ops.nim
@@ -20,39 +20,48 @@ template `==`*[A: SomeFloat, B: SomeInteger](a: A, b: B): bool = a == A(b)
 template `<>`*[A: SomeInteger, B: SomeFloat](a: A, b: B): bool = B(a) != b # Python 1.x and 2.x
 template `<>`*[A: SomeFloat, B: SomeInteger](a: A, b: B): bool = a != A(b) # Python 1.x and 2.x
 
-template `/`*(x, y: BiggestInt): float = system.`/`(int(x), int(y))
+template `/`*(x: SomeInteger, y: SomeInteger): float = system.`/`(float(x), float(y))
 
-func `//`*[A, B: SomeFloat | SomeInteger](a: A, b: B): int | float {.inline.} =
-  ## Python-like floor division
-  let data = floor(float(a) / float(b))
-  # Both arguments are float - result if float
-  when A is SomeFloat and B is SomeFloat: result = data
-  # Convert result to float
-  elif A is SomeFloat: result = A(data)
-  # Convert result to float
-  elif B is SomeFloat: result = B(data)
-  # Both arguments are int - result is int
-  else: result = int(data)
+type
+  ArithmeticError* = object of CatchableError
+  ZeroDivisionError* = object of ArithmeticError
 
-template `%`*[T: SomeNumber](a, b: T): T =
+template zeRaise(x) =
+  if x == typeof(x)(0):
+    raise newException(ZeroDivisionError, "division or modulo by zero")
+
+func `%`*[T: SomeNumber](a, b: T): T =
   ## Python-like modulo
-  a - b * (a // b)
+  runnableExamples:
+    assert 13 % -3 == -2
+    assert -13 % 3 == 2
+  zeRaise b
+  # Nim's `mod` is the same as `a - b * (a // b)` (i.e. remainder), while Py's is not.
+  floorMod a,b
 
 template `%`*[A: SomeFloat, B: SomeInteger](a: A, b: B): A = a % A(b)
 template `%`*[A: SomeInteger; B: SomeFloat](a: A, b: B): B = B(a) % b
+
+
+func `//`*[A, B: SomeFloat | SomeInteger](a: A, b: B): int | float {.inline.} =
+  ## Python-like floor division
+  runnableExamples:
+    assert 13 // -3 == -5
+    assert 13 div -3 == -4
+  when A is SomeInteger and B is SomeInteger:
+    (a - a % b) div b
+  else:
+    (a.float - a % b) / b.float
+
+func divmod*[T: SomeNumber](x, y: T): (T, T) = 
+  ## differs from std/math divmod
+  (x//y, x%y)
 
 template `==`*(a, b: typedesc): bool =
   ## Compare 2 typedesc like Python.
   runnableExamples: doAssert type(1) == type(2)
   a is b
 
-template degress*(x: SomeFloat): untyped =
-  ## https://devdocs.io/python/library/math#math.degrees
-  radToDeg(x)
-
-template radians*(x: SomeFloat): untyped =
-  ## https://devdocs.io/python/library/math#math.radians
-  degToRad(x)
 
 template id*(x): int =
   runnableExamples:
