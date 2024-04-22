ef51fd7edfd00363aafdacb82071317b7c631158
litlighilit <litlighilit@foxmail.com>
2024-04-22 20:23:37 +0800

    add `del ls[idx]`

--- src/pylib/pysugar.nim
+++ src/pylib/pysugar.nim
@@ -1,4 +1,6 @@
 
+import std/macros
+
 import ./pywith, ./classdef/class, ./classdef/pydef
 export class, pydef, pywith
 
@@ -20,3 +22,24 @@ template `:=`*(name, value: untyped): untyped =
   ## Creates new variable `name` and assign `value` to it.
   (var name = value; name)
 
+macro del*(seqIdx) =
+  ## - `del ls[idx]` -> `delete(ls, idx)`;
+  ## - `del obj.attr` -> compilation-error. Nim is static-typed,
+  ##  dynamically deleting object's attributes is not allowed.
+  ## 
+  ## NOTE: Nim's del(seq, idx) is an O(1) operation, 
+  ##  which moves the last element to `idx`
+  runnableExamples:
+    var ls = list([1,2,3,4,5])
+    del ls[2]
+    assert ls[2] == 4
+  if seqIdx.kind == nnkBracketExpr:
+    let
+      seqV = seqIdx[0]
+      idx  = seqIdx[1]
+    result = newCall("delete", seqV, idx)
+  # Maybe others will impl it.
+  #elif seqIdx.kind == nnkDotExpr: error "del obj.attr is unsupported in Nim"
+  else:
+    # fallback AS-IS
+    result = newCall("del", seqIdx)
