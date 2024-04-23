307a41686d665106b7b221b873c28c62a5f49793
litlighilit <litlighilit@foxmail.com>
2024-04-23 22:27:09 +0800

    add more `abc` in `collections`

--- src/pylib/collections_abc.nim
+++ src/pylib/collections_abc.nim
@@ -1,19 +1,67 @@
 
 
 type
-  Iterable*[T] = concept self  ## Mimic Pythons Iterable.
+  Iterable*[T] = concept self  ## Mimic Pythons Iterable. But not checks `iter`
     for value in self:
       value is T
   Sized* = concept self
-    self.len
+    len(self) is int
   
   Container*[T] = concept self
     contains(self, T) is bool
   
-  Collection*[T] = concept self of Sized, Container, Iterable
+  Collection*[T] = concept self of Sized, Container[T], Iterable[T]
   
-  Sequence*[T] = concept self of Collection
+  Sequence*[T] = concept self of Collection[T]
     self[int] is T
-
-
   
+  MutableSequence*[T] = concept self of Sequence[T]
+    self[int] = T
+    self.delitem(int)  ## __delitem__
+    self.insert(int, T)  ## insert item before index
+
+func contains*[T](s: Sequence[T], x: T): bool =
+  for i in s:
+    if x == i: return true
+
+#func reversed*[S: Sequence](s: S): Iterable
+
+func index*[T](s: Sequence[T], x: T, start=0, stop = -1): int =
+  let last = if stop == -1: s.len-1 else: stop 
+  result = start
+  for i in start..last:
+    if x == s[i]:
+      return result
+    result.inc
+  raise newException(ValueError, $x & " is not in sequence")
+
+func count*[T](s: Sequence[T], x: T): int =
+  for i in s:
+    if i == x:
+      result.inc
+
+func append*[T](ms: MutableSequence[T], x: T) =
+  ms.insert(ms.len, x)
+
+func clear*(ms: MutableSequence) = 
+  for i in 0..ms.len:
+    ms.delitem(i)
+    
+func reverse*(ms: MutableSequence) =
+  let le = ms.len
+  for i in 0 ..< le div 2:
+    swap ms[i], ms[le-1-i]
+
+func extend*[T](ms: MutableSequence[T], it: Iterable[T]) =
+  for i in it:
+    ms.append(i)
+
+func pop*[T](ms: MutableSequence[T]): T =
+  let last = ms.len-1
+  result = ms[last]
+  ms.delitem(last)
+
+func remove*[T](ms: MutableSequence[T], x: T) =
+  for i in 0..<ms.len:
+    if ms[i] == x:
+      ms.delitem(i)
