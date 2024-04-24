55a04c7590bd92b57e639335e904a4121dce4095
litlighilit <litlighilit@foxmail.com>
2024-04-24 16:36:56 +0800

    complex.real/imag instead of re/im

--- src/pylib/builtins/complex.nim
+++ src/pylib/builtins/complex.nim
@@ -4,21 +4,30 @@ import std/complex as ncomplex except im
 
 type PyComplex*[T] = distinct Complex[T]
 
+template toNimComplex*[T](z: PyComplex[T]): Complex[T] =
+  Complex[T] z
+
+template borrowAttr(expAs, attr) =
+  ## borrow postfix, assuming returns T
+  template expAs*[T](z: PyComplex[T]): T =
+    bind toNimComplex
+    z.toNimComplex.attr
+
+borrowAttr imag, im
+borrowAttr real, re
+
 func `$`*(z: PyComplex): string =
   let
-    real = z.re
-    imag = z.im
+    real = z.real
+    imag = z.imag
   result.add '('
   if real != 0:
-    result.add $z.re
+    result.add $real
     if imag >= 0:
       result.add '+'
     # if negative, `-` will be prefix-ed by `$`
   result.add $imag & 'j'
   result.add ')'
-
-template toNimComplex*[T](z: PyComplex[T]): Complex[T] =
-  Complex[T] z
 
 template pycomplex*[T](z: ncomplex.Complex[T]): PyComplex[T] =
   bind PyComplex
@@ -29,16 +38,6 @@ template pycomplex*[T](re, im: T): PyComplex[T] =
   ## alias of `complex`.
   ## Useful when import both std/complex and pylib
   complex(re, im)
-
-template borrowAttr(postfix) =
-  ## borrow postfix, assuming returns T
-  template postfix*[T](z: PyComplex[T]): T =
-    bind toNimComplex
-    z.toNimComplex.postfix
-
-borrowAttr im
-borrowAttr re
-
 
 func abs*[T](z: PyComplex[T]): T = abs(z.toNimComplex)  ## builtins.abs for complex
 func conjugate*[T](z: PyComplex[T]): PyComplex[T] = pycomplex conjugate(z.toNimComplex)
