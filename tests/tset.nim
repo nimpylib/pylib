ef33a9c38483078b7f57265ab6466cc5c6634a90
litlighilit <litlighilit@foxmail.com>
2024-05-05 22:40:16 +0800

    test: set shallow-copy

--- tests/tset.nim
+++ tests/tset.nim
@@ -1,5 +1,11 @@
 
 test "set":
+  def t_shallow():
+    s1 = set([1,2,3])
+    s2 = s1
+    s2.pop()
+    check(len(s1)==2)
+  t_shallow()
   def fun():
     s = pyset([2, 3])
     check(len(s.union([1]))==3)
@@ -11,4 +17,9 @@ test "set":
     check(len(s | pyset([1]))==3)
     check(len(s & pyset([1,2,3]))==2)
   op()
+
+  def lit():
+    s = pysetLit({1,2,3})
+    check(len(s) == 3)
+  lit()
     
