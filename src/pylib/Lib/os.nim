ab05767466737ec0197713ce00c53a53ac67c2f5
litlighilit <litlighilit@foxmail.com>
2024-05-06 10:13:19 +0800

    docs: os os.path.join can compile now

--- src/pylib/Lib/os.nim
+++ src/pylib/Lib/os.nim
@@ -2,16 +2,6 @@
 ## see docs.python.org/3/library/os.html
 ## 
 ## Also export everything of std/os
-## 
-## as the following will fail to compile:
-## 
-## ```Nim
-## import std/os
-## import pylib/Lib/os
-## os.path.join ...
-## ```
-## 
-## not completed yet
 
 import std/os
 export os
