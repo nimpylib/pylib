460901a0542b1d3a9a87dd2d3cd6a6affee3b6cd
litlighilit <litlighilit@foxmail.com>
2024-05-08 11:05:09 +0800

    fixup: split

--- src/pylib/pystring/split/common.nim
+++ src/pylib/pystring/split/common.nim
@@ -6,6 +6,13 @@ export strimpl
 # `PREALLOC_SIZE` (unicodeobject.c L17, used in L59) uses 12 as min len.
 const DefSplitCap* = 12
 
+# split.h PREALLOC_SIZE L17
 template PREPARE_CAP*(maxcount): int =
-  # split.h PREALLOC_SIZE L17
+  ## `maxcount` shall be Natural
   min(DefSplitCap, maxcount)
+
+# unicodeobject.c L9907
+func norm_maxsplit*(maxsplit: int, str_len, sep_len: int): int =
+  if maxsplit < 0:
+    result = if sep_len == 0: 0 else: (str_len div sep_len) + 1
+    if result < 0: result = str_len
