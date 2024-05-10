490c3d380c24f029da85cd5f8182b2a2bf95b5c9
litlighilit <litlighilit@foxmail.com>
2024-05-10 22:02:40 +0800

    fix: export os.scandir

--- src/pylib/Lib/os_impl/posix_like.nim
+++ src/pylib/Lib/os_impl/posix_like.nim
@@ -1,7 +1,7 @@
 ## posixmodule, also mimic posix api for Windows
 
 import ./posix_like/[
-  fdopen, open_close, truncate, stat
+  fdopen, open_close, truncate, stat, scandir
 ]
 
-export fdopen, open_close, truncate, stat
+export fdopen, open_close, truncate, stat, scandir
