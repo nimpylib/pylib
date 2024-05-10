72e5cba96e9601d85e87ebb20ab667cf758a6817
litlighilit <litlighilit@foxmail.com>
2024-05-10 21:08:55 +0800

    fix(py): hex/oct/bin now returns PyStr over string

--- src/pylib/pystring/strbltins.nim
+++ src/pylib/pystring/strbltins.nim
@@ -86,3 +86,36 @@ template ascii*(a: untyped): PyStr =
     assert ascii(6) == "6"
   bind pyasciiImpl, str
   str pyasciiImpl(repr(a))
+
+proc absInt[T: SomeSignedInt](x: T): T{.inline.} =
+  ## For JS,
+  ## Between nim's using BigInt and 
+  ## this [patch](https://github.com/nim-lang/Nim/issues/23378)
+  ##   `system.abs` will gen: `(-1)*x`, which can lead to a runtime err
+  ## as `x` may be a `bigint`, which causes error:
+  ##    Uncaught TypeError: Cannot mix BigInt and other types, ...
+  if x < 0.T: result = T(-1) * x
+  else: result = x
+
+template makeConv(name, call: untyped, len: int, pfix: string) =
+  func `name`*(a: SomeInteger): PyStr =
+    # Special case
+    if a == 0:
+      return `pfix` & "0"
+    var res = call(
+      when a isnot SomeUnsignedInt:
+        absInt(a)
+      else:
+        a,
+      `len`).toLowerAscii().strip(chars = {'0'}, trailing = false)
+    # Do it like in Python - add - sign
+    res.insert(`pfix`)
+    when a isnot SomeUnsignedInt: # optimization
+      if a < 0:
+        res.insert "-"
+    result = str res
+
+# Max length is derived from the max value for uint64
+makeConv(oct, toOct, 30, "0o")
+makeConv(bin, toBin, 70, "0b")
+makeConv(hex, toHex, 20, "0x")
