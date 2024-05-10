bb4b8abc643cb56776e92cf4927506cf8a8e2135
litlighilit <litlighilit@foxmail.com>
2024-05-10 22:18:29 +0800

    fix(ci): `ENOENT` is not `importc` when JS

--- src/pylib/pyerrors.nim
+++ src/pylib/pyerrors.nim
@@ -9,17 +9,19 @@ type
 # some error is still defined in ./io.nim
 # as they're currently only used there.
 
-when defined(windows):
-  let enoent = 2
-  let ERROR_PATH_NOT_FOUND = 3
-  proc isNotFound*(err: OSErrorCode): bool = 
-    let i = err.int
-    i == enoent or i == ERROR_PATH_NOT_FOUND
+when not defined(js):
+  when defined(windows):
+    let enoent = 2
+    let ERROR_PATH_NOT_FOUND = 3
+    proc isNotFound*(err: OSErrorCode): bool = 
+      let i = err.int
+      i == enoent or i == ERROR_PATH_NOT_FOUND
+  else:
+    let ENOENT{.importc, header: "<errno.h>".}: cint
+    let enoent = ENOENT.int
+    proc isNotFound*(err: OSErrorCode): bool = err.int == enoent
 else:
-  let ENOENT{.importc, header: "<errno.h>".}: cint
-  let enoent = ENOENT.int
-  proc isNotFound*(err: OSErrorCode): bool = err.int == enoent
-
+  proc isNotFound*(err: OSErrorCode): bool{.error: "not implement for JS backend".}
 
 func raiseFileNotFoundError*(fp: string) =
     raise newException(FileNotFoundError,
