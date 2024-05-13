db45a755cd502c9c8d322b19c1436b9166475f8d
litlighilit <litlighilit@foxmail.com>
2024-05-13 19:06:11 +0800

    feat: os.{make,remove}dirs

--- src/pylib/Lib/os_impl/utils.nim
+++ src/pylib/Lib/os_impl/utils.nim
@@ -4,12 +4,34 @@
 import std/os
 
 import ./common
-
+import ./posix_like/mkrmdir
 
 proc getcwd*(): PyStr = str getCurrentDir()
 proc getcwdb*(): PyBytes = bytes getCurrentDir()
 proc chdir*(s: PathLike) = setCurrentDir $s
 
+proc makedirs*[T](d: PathLike[T], mode=0o777, exists_ok=false) =
+  let dir = $d
+  if dir == "":
+    return
+  var omitNext = isAbsolute(dir)
+  for p in parentDirs(dir, fromRoot=true):
+    if omitNext:
+      omitNext = false
+    else:
+      if not exists_ok and dirExists(p):
+        raiseFileExistsError mapPathLike[T] p
+      mkdir(p)
 
-
-
+proc removedirs*(d: PathLike) =
+  let dir = $d
+  if dir == "":
+    return
+  # raises OSError if the leaf directory could not be successfully removed.
+  rmdir(d)  
+  var omitNext = isAbsolute(dir)
+  try:
+    for p in parentDirs(dir, inclusive=false):
+      rmdir(p)
+  except OSError:
+    discard
