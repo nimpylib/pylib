968a42633be58a905c462ebd1bf3b78dd8ac952d
litlighilit <litlighilit@foxmail.com>
2024-05-13 22:31:53 +0800

    refine: ren scandir.nim scandirImpl.nim
    to not export scandirIter in posix_like

--- src/pylib/Lib/os_impl/posix_like.nim
+++ src/pylib/Lib/os_impl/posix_like.nim
@@ -1,7 +1,7 @@
 ## posixmodule, also mimic posix api for Windows
 
 import ./posix_like/[
-  fdopen, open_close, truncate, stat, scandir, mkrmdir
+  fdopen, open_close, truncate, stat, scandirImpl, mkrmdir
 ]
 
-export fdopen, open_close, truncate, stat, scandir, mkrmdir
+export fdopen, open_close, truncate, stat, scandirImpl.scandir, mkrmdir
