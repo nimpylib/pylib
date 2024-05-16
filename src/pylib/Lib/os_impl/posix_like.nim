93ed02d588760c5a38f94174b8bc55195bc4a328
litlighilit <litlighilit@foxmail.com>
2024-05-16 18:16:10 +0800

    feat: os.{rename,replace}

--- src/pylib/Lib/os_impl/posix_like.nim
+++ src/pylib/Lib/os_impl/posix_like.nim
@@ -1,7 +1,10 @@
 ## posixmodule, also mimic posix api for Windows
 
 import ./posix_like/[
-  fdopen, open_close, truncate, stat, scandirImpl, mkrmdir, unlink
+  fdopen, open_close, truncate, stat, scandirImpl, mkrmdir, unlink,
+  rename
 ]
 
-export fdopen, open_close, truncate, stat, scandirImpl.scandir, mkrmdir, unlink
+export
+  fdopen, open_close, truncate, stat, scandirImpl.scandir, mkrmdir, unlink,
+  rename
---
+++ src/pylib/Lib/os_impl/posix_like/rename.nim
@@ -0,0 +1,27 @@
+
+include ./ncommon
+import ../common
+
+# translated from CPython-3.13-alpha/Modules/posixmodule.c L5674
+# internal_rename
+when defined(windows):
+  proc internal_rename[T](src, dst: PathLike[T], is_replace: static[bool]): bool =
+    let flag = when is_replace: MOVEFILE_REPLACE_EXISTING else: 0
+    let s = newWideCString($src)
+    let d = newWideCString($dst)
+    result = moveFileExW(s, d, flag.DWORD) != 0'i32
+else:
+  proc c_rename(src, dst: cstring): cint{.importc: "rename", header: "<stdio.h>".}
+  proc internal_rename[T](src, dst: PathLike[T], is_replace: static[bool]): bool =
+    result = c_rename(cstring $src, cstring $dst) == 0.cint
+
+template raiseExcWithPath2(src, dst: PathLike) =
+  raiseExcWithPath($src & " -> " & $dst)
+
+proc rename*[T](src, dst: PathLike[T]) =
+  if not internal_rename(src, dst, false):
+    raiseExcWithPath2(src, dst)
+
+proc replace*[T](src, dst: PathLike[T]) = 
+  if not internal_rename(src, dst, true):
+    raiseExcWithPath2(src, dst)
