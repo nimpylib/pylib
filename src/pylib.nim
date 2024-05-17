d37336e3f5b369a2d23e7a4d09ff73171c838b8c
litlighilit <litlighilit@foxmail.com>
2024-05-17 20:13:23 +0800

    fixup: cleanup sys/platform part in pylib.nim

--- src/pylib.nim
+++ src/pylib.nim
@@ -23,33 +23,6 @@ when not defined(pylibNoLenient):
   import std/lenientops
   export lenientops
 
-
-const
-  platform* = (system: hostOS, machine: hostCPU, processor: hostCPU)  ## Platform info.
-  version_info* = (
-    major: NimMajor,
-    minor: NimMinor,
-    micro: NimPatch,
-    releaselevel: "final",
-    serial: 0
-  )  ## Version information (SemVer).
-  sys* = (
-    platform:     hostOS,
-    maxsize:      high(BiggestInt),
-    version:      NimVersion,
-    version_info: version_info,
-    byteorder:    $cpuEndian,
-    copyright:    "MIT",
-    hexversion:   NimVersion.toHex.toLowerAscii(),
-    api_version:  NimVersion
-  )  ## From http://devdocs.io/python~3.7/library/sys
-
-type
-  Platform* = typeof(platform)
-  VersionInfo* = typeof(version_info)
-  Sys* = typeof(sys)
-
-
 template timeit*(repetitions: int, statements: untyped): untyped =
   ## Mimics Pythons ``timeit.timeit()``, output shows more information than Pythons.
   bind times.`$`
