b4f85eb645f4e5e38517df1afbfe238c5518b593
litlighilit <litlighilit@foxmail.com>
2024-06-07 16:26:06 +0800

    break: dict now is ref object; rm TableLike; fix repr, etc

--- src/pylib/builtins/dict.nim
+++ src/pylib/builtins/dict.nim
@@ -3,15 +3,10 @@ import std/tables
 import std/macros
 from ../collections_abc import Iterable
 
-type 
-  TableLikeObj* = Table or OrderedTable or CountTable
-  TableLikeRef* = TableRef or OrderedTableRef or CountTableRef
-  TableLike* = TableLikeObj or TableLikeRef
-
 # Impl begin
-
 type
-  PyDict*[K, V] = distinct OrderedTableRef[K, V]
+  PyDict*[K, V] = ref object
+    data: OrderedTable[K, V]
   PyDictView* = object of RootObj ## .. warning:: currently `mapping` attr
                                   ## is dict itself, i.e. modifiable
   PyDictKeyView*[T] = object of PyDictView
@@ -24,14 +19,23 @@ type
   SomePyDictView* = PyDictKeyView | PyDictValueView | PyDictItemView
 
 
-template toNimTable(self: PyDict): OrderedTableRef = OrderedTableRef(self)
+template toNimTable(self: PyDict): OrderedTable = self.data
+template newPyDictImpl[K, V](x: varargs): untyped =
+  ## zero or one arg
+  ## shall support `[]`, `{k:v}`, `@[(k, v),...]`
+  bind newOrderedTable
+  PyDict[K, V](data: toOrderedTable[K, V](x))
+
+proc getOrDefault[A, B](t: PyDict[A, B], key: A): B =
+  ## inner. used to impl get(key, default)
+  t.toNimTable.getOrDefault key
+func emptyPyDict*[K, V](): PyDict[K, V] = newPyDictImpl[K, V]([])
+# Impl end
+
 proc contains*[A, B](t: PyDict[A, B], key: A): bool = contains(t.toNimTable, key)
 proc `[]`*[A, B](t: PyDict[A, B], key: A): B = `[]`(t.toNimTable, key)
 proc `[]=`*[A, B](t: PyDict[A, B], key: A, val: sink B) =
   `[]=`(t.toNimTable, key, val)
-proc getOrDefault[A, B](t: PyDict[A, B], key: A): B =
-  ## inner. used to impl get(key, default)
-  t.toNimTable.getOrDefault key
 
 using self: PyDict
 
@@ -41,7 +45,7 @@ template strIterImpl(view, strProc;
   var result = newStringOfCap(2+3*le)
   result.add start
   var i = 0
-  for k in view:
+  for k in iter(view):
     if i == le - 1:
       result.add k.strProc
       break
@@ -75,8 +79,8 @@ template iter*(view: SomePyDictView): untyped = view.mapping.iter
 template items*(view: SomePyDictView): untyped = view.iter
 func contains*[T](t: PyDictKeyView[T], x: T): bool = contains(t.mapping, x)
 func contains*[T](t: PyDictValueView[T], x: T): bool = 
-  for k in t.mapping:
-    if t.mapping[k] == x: return true
+  for v in t.mapping.values():
+    if v == x: return true
 func contains*[K, V](t: PyDictItemView[K, V], x: (K, V)): bool = 
   if x[0] in t.mapping:
     return x[1] == t.mapping[x[0]]
@@ -96,7 +100,7 @@ genRepr PyDictItemView,  "dict_items"
 
 
 iterator keys*[K, V](self: PyDict[K ,V]): K =
-  for i in self.toNimTable: yield i
+  for i in self.toNimTable.keys(): yield i
 
 iterator iter*[K, V](self: PyDict[K ,V]): K =
   ## .. warning:: Nim's for stmt calls `items` implicitly, instead of iter
@@ -108,24 +112,18 @@ iterator iter*[K, V](self: PyDict[K ,V]): K =
     for i in d:
       assert i == ("a", 1)
   
-  for i in self.keys: yield i
+  for i in self.keys(): yield i
 
 
 iterator values*[K, V](self: PyDict[K ,V]): V =
-  for i in self.toNimTable.values: yield i
+  for i in self.toNimTable.values(): yield i
 
 iterator items*[K, V](self: PyDict[K ,V]): (K, V) =
-  for i in self.toNimTable.pairs: yield i
+  for i in self.toNimTable.pairs(): yield i
 
 func keys*[K, V](self: PyDict[K ,V]): PyDictKeyView[K] = result.mapping = self
 func values*[K, V](self: PyDict[K ,V]): PyDictValueView[V] = result.mapping = self
 func items*[K, V](self: PyDict[K ,V]): PyDictItemView[K, V] = result.mapping = self
-
-template newPyDictImpl[K, V](x: varargs): untyped =
-  ## zero or one arg
-  ## shall support `[]`, `{k:v}`, `@[(k, v),...]`
-  bind newOrderedTable
-  PyDict newOrderedTable[K, V](x)
 
 func toPyDict*[K, V](x: openArray[(K, V)]): PyDict[K, V] =
   # NOTE: in Nim, `{k:v, ...}` is of `array[(type(k), type(v))]`
@@ -152,27 +150,28 @@ func setdefault*[K, V](self: PyDict[K, V], key: K, default = V.default) =
   return default
 
 func get*[K, V](self: PyDict[K ,V], key: K): V = self[key]
-func get*[K, V](self: PyDict[K ,V], key: V, default: V): V = self.getOrDefault(key, default)
+func get*[K, V](self: PyDict[K ,V], key: K, default: V): V = self.getOrDefault(key, default)
 func pop*[K, V](self: PyDict[K, V], key: K): V = 
   if not self.pop(key, result):
     raise newException(KeyError, $key)
 func pop*[K, V](self: PyDict[K, V], key: K, default: V): V = 
-  if not self.pop(key, result):
+  if not self.toNimTable.pop(key, result):
     result = default
 
 func popitem*[K, V](self: PyDict[K, V]): (K, V) =
-  result[0] = self.keys()()
+  ## .. warning:: this is currently LILO instead of LIFO
+  for i in self.toNimTable.keys():
+    result[0] = i
+    break
   discard self.pop(result[0], result[1])
 
 proc delitem*[K, V](self: PyDict[K, V], k: K) =
   ## pysugar expect such a proc to hook `del d[k]`
   self.toNimTable.del k
 
-func emptyPyDict*[K, V](): PyDict[K, V] = newPyDictImpl[K, V]([])
 
 template PyDictProc: NimNode = ident "toPyDict" # the proc must be exported
 
-# Impl end
 
 proc parseKeyValues(kwargs: NimNode|seq[NimNode]): NimNode =
   # k=v,... -> [("k", v),...]
@@ -229,66 +228,26 @@ macro update*(self: PyDict, kws: varargs[untyped]) =
   let setitem = bindSym"[]="
   if is1stIter:
     result.add quote do:
-      for t in `first`:
-        `setitem` `self`, t[0], t[1]
+      when compiles(`first`.keys()):
+        for k in `first`.keys():
+          `setitem` `self`, k, `first`[k]
+      else:
+        for t in `first`:
+          `setitem` `self`, t[0], t[1]
     startKw = 1
   for kv in kws[startKw..^1]:
     result.add newCall(setitem, self, newLit $kv[0], kv[1])
   
-func `|`*[A, B: TableLike](a: A, b: B): A =
-  ## Python-like merge dict operator `print({"a":1} | {"b":2})`,
+func `|`*(a, b: PyDict): PyDict =
+  ## Python-like merge dict operator,
   ## a new dict is created from `a` and `b`, keys in the second 
   ## operand override keys in the first operand 
-  runnableExamples:
-    import std/tables
-
-    let d = {"spam": "1", "eggs": "2", "cheese": "3"}.toTable
-    let e = {"cheese": "cheddar", "aardvark": "Ethel"}.toTable
-    doAssert d | e == {"eggs": "2", "spam": "1", "cheese": "cheddar", "aardvark": "Ethel"}.toTable
-    doAssert e | d == {"eggs": "2", "spam": "1", "cheese": "3", "aardvark": "Ethel"}.toTable
-
-    let x = {"a": "0", "b": "1"}.toOrderedTable
-    let y = {"c": "2", "b": "1"}.toOrderedTable
-    doAssert x | y == {"a": "0", "b": "1", "c": "2"}.toOrderedTable
-
-    let z = {"a": "0", "b": "1"}.toCountTable
-    let v = {"c": "2", "b": "1"}.toCountTable
-    doAssert z | v  == {"a": "0", "b": "1", "c": "2"}.toCountTable
-  
-  when A is ref|ptr:
-    new result
-  for key, val in a.pairs:
-    result[key] = val
-  
-  for key, val in b.pairs:
+  result = a.copy()
+  for key, val in b.items():
     result[key] = val
 
-# TableRef and similar don't need a "var" to be modified
-proc `|=`*[A: TableLikeRef, B: TableLike](a: A, b: B) = 
+proc `|=`*(a, b: PyDict) = 
   ## Python-like in-place dict update operator.
   ## `b` is added into `a`, keys in `b` override same keys from `a`
-  runnableExamples:
-    import std/tables
-
-    let d = {"spam": "1", "eggs": "2", "cheese": "3"}.newTable
-    let e = {"cheese": "cheddar", "aardvark": "Ethel"}.newTable
-    d |= e
-    doAssert d == {"spam": "1", "eggs": "2", "aardvark": "Ethel", "cheese": "cheddar"}.newTable
-  
-  for key, val in b.pairs:
-    a[key] = val
-
-# Table and similar however need it
-func `|=`*[A: TableLikeObj, B: TableLike](a: var A, b: B) = 
-  ## Python-like in-place dict update operator.
-  ## `b` is added into `a`, keys in `b` override same keys from `a`
-  runnableExamples:
-    import std/tables
-
-    var d = {"spam": "1", "eggs": "2", "cheese": "3"}.toTable
-    let e = {"cheese": "cheddar", "aardvark": "Ethel"}.newTable
-    d |= e
-    doAssert d == {"spam": "1", "eggs": "2", "aardvark": "Ethel", "cheese": "cheddar"}.toTable
-
-  for key, val in b.pairs:
+  for key, val in b.items():
     a[key] = val
