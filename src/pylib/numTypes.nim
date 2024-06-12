79937f311e528fd2b99627116b1bea9e8eda1375
litlighilit <litlighilit@foxmail.com>
2024-06-12 13:13:08 +0800

    style: numTypes: norm resType

--- src/pylib/numTypes.nim
+++ src/pylib/numTypes.nim
@@ -1,14 +1,22 @@
 
 import std/strutils
 
-template int*(a: string): BiggestInt = parseBiggestInt(a)
-template int*(a: char): BiggestInt = parseBiggestInt($a)
-template int*[T: SomeNumber](a: T): system.int = system.int(a)
-template int*(a: bool): int = (if a: 1 else: 0)
+type Int* = system.int  ## alias of system.int
+
+template int*(a: string): Int =
+  bind parseInt
+  parseInt(a)
+template int*(a: char): Int =
+  bind parseInt
+  parseInt($a)
+template int*(a: bool): Int = (if a: 1 else: 0)
+
 template long*(a: string): BiggestInt = parseBiggestInt(a)
 template long*(a: char): BiggestInt = parseBiggestInt($a)
-template long*[T: SomeNumber](a: T): system.int = system.int(a)
-template long*(a: bool): int = (if a: 1 else: 0)
-template float*(a: string): BiggestFloat = parseFloat(a)
-template float*[T: SomeNumber](a: T): system.float = system.float(a)
-template float*(a: bool): float = (if a: 1.0 else: 0.0)
+template long*[T: SomeNumber](a: T): BiggestInt = BiggestInt(a)
+template long*(a: bool): int = BiggestInt(if a: 1 else: 0)
+
+template float*(a: string): BiggestFloat =
+  bind parseFloat
+  parseFloat(a)
+template float*(a: bool): BiggestFloat = (if a: 1.0 else: 0.0)
