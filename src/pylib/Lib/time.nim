351ae40b20bf8776c4dd585116a3f85fe54f7c0f
litlighilit <litlighilit@foxmail.com>
2024-06-18 13:15:19 +0800

    fix(py): time.sleep: raises ValueError if s < 0 (Nim in win just deadloop)

--- src/pylib/Lib/time.nim
+++ src/pylib/Lib/time.nim
@@ -3,11 +3,20 @@
 # status: not tested yet; not finished
 
 import std/os as nos
+template sleep_neg_raise(s) =
+  ## As of 2.1.1, Nim's std/os sleep will deadloop in Windows if `milsecs` is negative.
+  if s < 0:
+    raise newException(ValueError, "sleep length must be non-negative")
+
 template sleep*(s: int) =
-  bind sleep
+  ## raises ValueError if s < 0
+  bind sleep, sleep_neg_raise
+  sleep_neg_raise(s)
   sleep(milsecs=1000 * s)  # param name based overload
 template sleep*(s: float) =
-  bind sleep
+  ## raises ValueError if s < 0
+  bind sleep, sleep_neg_raise
+  sleep_neg_raise(s)
   sleep(milsecs=int(1000 * s))
 
 
