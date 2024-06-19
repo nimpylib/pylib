1bddc386a6501f3c694003c44d8c3114696127ab
litlighilit <litlighilit@foxmail.com>
2024-06-19 21:45:19 +0800

    fix: time.[as]ctime wrong minutes

--- src/pylib/Lib/time_impl/asctimeImpl.nim
+++ src/pylib/Lib/time_impl/asctimeImpl.nim
@@ -7,7 +7,7 @@ template asctimeImpl*(result: string, dt: DateTime) =
   var day = dt.format "dd "
   if day[0] == '0': day[0] = ' '
   result.add day
-  result.add dt.format "HH:MM:ss uuuu"
+  result.add dt.format "HH:mm:ss uuuu"
 
 func asctime*(dt: DateTime): string =
   asctimeImpl(result, dt)
