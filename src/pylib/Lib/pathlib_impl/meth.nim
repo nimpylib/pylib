7b88c490c71d15a4598efc1a2b819f4f7eacd0cb
litlighilit <litlighilit@foxmail.com>
2024-06-26 19:04:33 +0800

    feat: pathlib.Path rel/abs relative methods

--- src/pylib/Lib/pathlib_impl/meth.nim
+++ src/pylib/Lib/pathlib_impl/meth.nim
@@ -1,8 +1,33 @@
 
 
 import ./types
+import std/os
+import std/strutils
 
 using self: types.Path
+
+func is_relative_to*(self; other: string): bool =
+  {.noSideEffect.}:
+    isRelativeTo($self, other)
+
+func is_relative_to*(self; other: Path): bool = self.is_relative_to $other
+
+func relative_to*(self; other: string|Path): Path =
+  Path relativePath($self, $other)
+
+proc absolute*(self): Path = Path absolutePath $self
+
+func is_absolute*(self): bool = isAbsolute $self
+
+func as_posix*(self): Path =
+  Path replace($self, DirSep, '/')
+
+#[ TODO(after urllib.parse.quote_from_bytes)
+func as_uri*(self): Path =
+  if not self.is_absolute():
+    raise newException(
+      ValueError, "relative path can't be expressed as a file URI")
+]#
 
 func `/`*(head: string, tail: Path): Path = Path(head) / tail
 func `/`*(head: Path, tail: string): Path = head / Path(tail)
