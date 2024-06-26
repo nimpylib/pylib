4150d3da54b9a5423692df4ba952d415967abd39
litlighilit <litlighilit@foxmail.com>
2024-06-26 20:11:34 +0800

    fix os.unlink from func to proc

--- src/pylib/Lib/os_impl/posix_like/unlink.nim
+++ src/pylib/Lib/os_impl/posix_like/unlink.nim
@@ -5,7 +5,7 @@ template unlinkBody(p) =
   if not unlinkImpl(p):
     p.raiseExcWithPath()
 
-func unlink*[T](p: PathLike[T]) = unlinkBody p
+proc unlink*[T](p: PathLike[T]) = unlinkBody p
 
 func remove*[T](p: PathLike[T]) =
   ## This function is semantically identical to `unlink`_
