be8f3aff1ef0f296cca88d0f828d3f8a2702dbcc
litlighilit <litlighilit@foxmail.com>
2024-06-26 19:11:26 +0800

    feat: pathlib: some of `is_*`

--- src/pylib/Lib/pathlib_impl/meth.nim
+++ src/pylib/Lib/pathlib_impl/meth.nim
@@ -6,18 +6,24 @@ import std/strutils
 
 using self: types.Path
 
-func is_relative_to*(self; other: string): bool =
-  {.noSideEffect.}:
-    isRelativeTo($self, other)
+proc is_relative_to*(self; other: string): bool =
+  isRelativeTo($self, other)
 
-func is_relative_to*(self; other: Path): bool = self.is_relative_to $other
+proc is_relative_to*(self; other: Path): bool = self.is_relative_to $other
 
 func relative_to*(self; other: string|Path): Path =
   Path relativePath($self, $other)
 
 proc absolute*(self): Path = Path absolutePath $self
 
-func is_absolute*(self): bool = isAbsolute $self
+template wrapPred(op, nop){.dirty.} =
+  proc op*(self): bool = nop $self
+
+wrapPred is_file, fileExists
+wrapPred is_dir, dirExists
+wrapPred is_symlink, symlinkExists
+
+wrapPred is_absolute, isAbsolute
 
 func as_posix*(self): Path =
   Path replace($self, DirSep, '/')
@@ -28,6 +34,9 @@ func as_uri*(self): Path =
     raise newException(
       ValueError, "relative path can't be expressed as a file URI")
 ]#
+
+func samefile*(self; other_path: string|Path): bool =
+  sameFile($self, $other_path)
 
 func `/`*(head: string, tail: Path): Path = Path(head) / tail
 func `/`*(head: Path, tail: string): Path = head / Path(tail)
