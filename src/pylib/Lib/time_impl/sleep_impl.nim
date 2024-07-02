65ec9eb3ec12d0a6401849fcc280dfa4d1c869aa
litlighilit <litlighilit@foxmail.com>
2024-07-02 15:51:46 +0800

    fix: time.sleep(s) evaluates `s` twice; update comment

--- src/pylib/Lib/time_impl/sleep_impl.nim
+++ src/pylib/Lib/time_impl/sleep_impl.nim
@@ -1,6 +1,7 @@
 
 template sleep_neg_raise(s) =
-  ## As of 2.1.1, Nim's std/os sleep will deadloop in Windows if `milsecs` is negative.
+  # beaware: before Nim#23734, (in 2.1.1),
+  # Nim's std/os sleep will deadloop in Windows if `milsecs` is negative.
   if s < 0:
     raise newException(ValueError, "sleep length must be non-negative")
 when not defined(js):
@@ -17,18 +18,14 @@ else:
     ## but most of the nodejs's api is for async...
     {.emit:"""
   const start = new Date().getTime();
-  let currentTime = null;
+  let currentTime;
   do {
     currentTime = new Date().getTime();
   } while (currentTime - start < `milsecs`);
     """.}
-template sleep*(s: int) =
+template sleep*(s: int|float) =
   ## raises ValueError if s < 0
   bind sleep, sleep_neg_raise
-  sleep_neg_raise(s)
-  sleep(milsecs=1000 * s)  # param name based overload
-template sleep*(s: float) =
-  ## raises ValueError if s < 0
-  bind sleep, sleep_neg_raise
-  sleep_neg_raise(s)
-  sleep(milsecs=int(1000 * s))
+  let ss = s  # prevent `s` being eval-ed twice.
+  sleep_neg_raise(ss)
+  sleep(milsecs=int(1000 * ss))  # param name based overload
