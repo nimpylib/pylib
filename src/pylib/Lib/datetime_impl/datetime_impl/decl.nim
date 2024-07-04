106f955b282dd3aebbed0411c22965c51783fa5d
litlighilit <litlighilit@foxmail.com>
2024-07-05 01:23:34 +0800

    func(Lib): datetime attrs

--- src/pylib/Lib/datetime_impl/datetime_impl/decl.nim
+++ src/pylib/Lib/datetime_impl/datetime_impl/decl.nim
@@ -12,3 +12,19 @@ func newDatetime*(dt: Datetime, tzinfo: tzinfo = nil): datetime =
 
 func asNimDatetime*(self: datetime): DateTime = self.dt
 
+using self: datetime
+template wrap(dtA, DtA){.dirty.} =
+  func dtA*(self): int = self.dt.DtA
+template wrap(dtA) = wrap(dtA, dtA)
+
+template wrap(dtA, DtA, cvt){.dirty.} =
+  func dtA*(self): int = self.dt.DtA.cvt
+
+wrap year
+wrap month, month, ord
+wrap day, monthday
+wrap hour
+wrap minute
+wrap second
+template ns2us(ns): untyped = ns div 1000
+wrap microsecond, nanosecond, ns2us
--- src/pylib/Lib/test/test_datetime.nim
+++ src/pylib/Lib/test/test_datetime.nim
@@ -2,14 +2,15 @@
 import ./import_utils
 importTestPyLib datetime
 
-test "utcoffset":
-  let
-    ZERO = timedelta(0)
-    MINUTE = timedelta(minutes=1)
-    HOUR = timedelta(hours=1)
-    DAY = timedelta(days=1)
-    DT = datetime(1970, 1, 1)
-  template assertEqual(a, b) = check a == b 
+let
+  ZERO = timedelta(0)
+  MINUTE = timedelta(minutes=1)
+  HOUR = timedelta(hours=1)
+  DAY = timedelta(days=1)
+  DT = datetime(1970, 1, 1)
+
+template assertEqual(a, b) = check a == b 
+test "datetime.utcoffset":
   def test_utcoffset():
     dummy = DT
     for h in [0.0, 1.5, 12.0]:
@@ -17,3 +18,16 @@ test "utcoffset":
         assertEqual(offset, timezone(offset).utcoffset(dummy))
         assertEqual(-offset, timezone(-offset).utcoffset(dummy))
   test_utcoffset()
+
+test "datetime attrs":
+  def test_trivial():
+    dt = datetime(1, 2, 3, 4, 5, 6, 7)
+    assertEqual(dt.year, 1)
+    assertEqual(dt.month, 2)
+    assertEqual(dt.day, 3)
+    assertEqual(dt.hour, 4)
+    assertEqual(dt.minute, 5)
+    assertEqual(dt.second, 6)
+    assertEqual(dt.microsecond, 7)
+    assertEqual(dt.tzinfo, None)
+  test_trivial()
