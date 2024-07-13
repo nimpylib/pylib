272b12490e97d715ede9d1a5e406df2756b8a7d8
litlighilit <litlighilit@foxmail.com>
2024-07-13 23:24:52 +0800

    func(Lib): datetime for py-like

--- src/pylib/Lib/datetime.nim
+++ src/pylib/Lib/datetime.nim
@@ -1,17 +1,38 @@
 
 import ./n_datetime
 
-export n_datetime except tzname
+export n_datetime except tzname, isoformat, strftime, ctime
 
+import ../pystring/strimpl
 import ../noneType
+import ../pyerrors/simperr
 
 
-#func tzname
+method tzname*(tz: tzinfo; dt: datetime): PyStr{.base.} =
+  str n_datetime.tzname(tz, dt)
+method tzname*(tz: timezone; dt: datetime): PyStr =
+  str n_datetime.tzname(tz, dt)
+func tzname*(dt: datetime): PyStr =
+  ## .. hint:: this won't returns `None`, but may return a empty string
+  str n_datetime.tzname(dt)
 
-
+using self: datetime # | date
+func strftime*(self; format: PyStr): PyStr = str n_datetime.strftime(self, $format)
+func isoformat*(self; sep: StringLike = 'T', timespec="auto"): PyStr =
+  template lenErr =
+    raise newException(TypeError, "isoformat() argument 1 must be a unicode character, not str")
+  when sep is string:
+    let sep = str sep    
+  if sep.len != 1: lenErr()
+  when sep is char:
+    let ch = sep
+  else:
+    let ch = $sep
+  str n_datetime.isoformat(self, sep=ch, timespec=timespec)
+func str*(self): PyStr = self.isoformat(' ')
+func ctime*(self): PyStr = str n_datetime.ctime(self)
 
 func `==`*(tzinfo: tzinfo, n: NoneType): bool = tzinfo.isTzNone
+func `==`*(delta: timedelta, n: NoneType): bool = delta.isTimeDeltaNone
 
-
-
-
+converter noneToTzInfo*(_: NoneType): tzinfo = TzNone
