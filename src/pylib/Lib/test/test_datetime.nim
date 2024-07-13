4d315cd2b0e3d9a13989f9d0803e417db35b4616
litlighilit <litlighilit@foxmail.com>
2024-07-13 11:33:48 +0800

    test: add for isoformat,ctime; refactor

--- src/pylib/Lib/test/test_datetime.nim
+++ src/pylib/Lib/test/test_datetime.nim
@@ -9,52 +9,54 @@ let
   DAY = timedelta(days=1)
   DT = datetime(1970, 1, 1)
 
-test "timedelta init":
-  template eq(a, b) = check a == b
-
-  # Check keyword args to constructor
-  eq(timedelta(1), timedelta(days=1))
-  eq(timedelta(0, 1), timedelta(seconds=1))
-  eq(timedelta(0, 0, 1), timedelta(microseconds=1))
-  eq(timedelta(weeks=1), timedelta(days=7))
-  eq(timedelta(days=1), timedelta(hours=24))
-  eq(timedelta(hours=1), timedelta(minutes=60))
-  eq(timedelta(minutes=1), timedelta(seconds=60))
-  eq(timedelta(seconds=1), timedelta(milliseconds=1000))
-  eq(timedelta(milliseconds=1), timedelta(microseconds=1000))
-
-test "timedelta float init":
-  template eq(a, b) = check a == b
-  # from CPython/tests/datetimetester.py
-  eq(timedelta(weeks=1.0/7), timedelta(days=1))
-  eq(timedelta(days=1.0/24), timedelta(hours=1))
-  eq(timedelta(hours=1.0/60), timedelta(minutes=1))
-  eq(timedelta(minutes=1.0/60), timedelta(seconds=1))
-  eq(timedelta(seconds=0.001), timedelta(milliseconds=1))
-  eq(timedelta(milliseconds=0.001), timedelta(microseconds=1))
-
 template assertEqual(a, b) = check a == b
-test "datetime.utcoffset":
-  def test_utcoffset():
-    dummy = DT
-    for h in [0.0, 1.5, 12.0]:
-        offset = h * HOUR
-        assertEqual(offset, timezone(offset).utcoffset(dummy))
-        assertEqual(-offset, timezone(-offset).utcoffset(dummy))
-  test_utcoffset()
 
-test "datetime attrs":
-  def test_trivial():
-    dt = datetime(1, 2, 3, 4, 5, 6, 7)
-    assertEqual(dt.year, 1)
-    assertEqual(dt.month, 2)
-    assertEqual(dt.day, 3)
-    assertEqual(dt.hour, 4)
-    assertEqual(dt.minute, 5)
-    assertEqual(dt.second, 6)
-    assertEqual(dt.microsecond, 7)
-    assertEqual(dt.tzinfo, None)
-  test_trivial()
+suite "datetime":
+  test "utcoffset":
+    def test_utcoffset():
+      dummy = DT
+      for h in [0.0, 1.5, 12.0]:
+          offset = h * HOUR
+          assertEqual(offset, timezone(offset).utcoffset(dummy))
+          assertEqual(-offset, timezone(-offset).utcoffset(dummy))
+    test_utcoffset()
+
+  test "attrs":
+    def test_trivial():
+      dt = datetime(1, 2, 3, 4, 5, 6, 7)
+      assertEqual(dt.year, 1)
+      assertEqual(dt.month, 2)
+      assertEqual(dt.day, 3)
+      assertEqual(dt.hour, 4)
+      assertEqual(dt.minute, 5)
+      assertEqual(dt.second, 6)
+      assertEqual(dt.microsecond, 7)
+      assertEqual(dt.tzinfo, None)
+    test_trivial()
+  
+  test "isoformat":
+    let t = datetime(1, 2, 3, 4, 5, 1, 123)
+    assertEqual(t.isoformat(),    "0001-02-03T04:05:01.000123")
+    assertEqual(t.isoformat("T"), "0001-02-03T04:05:01.000123")
+    assertEqual(t.isoformat(" "), "0001-02-03 04:05:01.000123")
+    assertEqual(t.isoformat("\x00"), "0001-02-03\x0004:05:01.000123")
+    # bpo-34482: Check that surrogates are handled properly.
+    assertEqual(t.isoformat("\ud800"),
+                      "0001-02-03\ud80004:05:01.000123")
+    assertEqual(t.isoformat(timespec="hours"), "0001-02-03T04")
+    assertEqual(t.isoformat(timespec="minutes"), "0001-02-03T04:05")
+    assertEqual(t.isoformat(timespec="seconds"), "0001-02-03T04:05:01")
+    assertEqual(t.isoformat(timespec="milliseconds"), "0001-02-03T04:05:01.000")
+    assertEqual(t.isoformat(timespec="microseconds"), "0001-02-03T04:05:01.000123")
+    assertEqual(t.isoformat(timespec="auto"), "0001-02-03T04:05:01.000123")
+    assertEqual(t.isoformat(sep=" ", timespec="minutes"), "0001-02-03 04:05")
+    expect(ValueError):
+      discard t.isoformat(timespec="foo")
+    # bpo-34482: Check that surrogates are handled properly.
+    expect(ValueError):
+      discard t.isoformat(timespec="\ud800")
+    # str is ISO format with the separator forced to a blank.
+    assertEqual(str(t), "0001-02-03 04:05:01.000123")
 
 class FixedOffset(tzinfo):
     offset: timedelta
@@ -75,23 +77,30 @@ class FixedOffset(tzinfo):
     def dst(self, dt: datetime):
         return self.dstoffset
 
-importPyLib time
-test "tzinfo fromtimestamp":
-  template meth(xs: varargs[untyped]): datetime =
-    datetime.PyDatetime.fromtimestamp(xs)
-  def test_tzinfo_fromtimestamp():
-    ts = time.time()
-    # Ensure it doesn't require tzinfo (i.e., that this doesn't blow up).
-    base = meth(ts)
-    # Try with and without naming the keyword.
-    off42 = newFixedOffset(42, "42")
-    another = meth(ts, off42)
-    again = meth(ts, tz=off42)
-    #assertIs(another.tzinfo, again.tzinfo)
-    assertEqual(another.utcoffset(), timedelta(minutes=42))
-  test_tzinfo_fromtimestamp()
+suite "timedelta":
+  test "init":
+    template eq(a, b) = check a == b
 
-suite "TestTimedelta":
+    # Check keyword args to constructor
+    eq(timedelta(1), timedelta(days=1))
+    eq(timedelta(0, 1), timedelta(seconds=1))
+    eq(timedelta(0, 0, 1), timedelta(microseconds=1))
+    eq(timedelta(weeks=1), timedelta(days=7))
+    eq(timedelta(days=1), timedelta(hours=24))
+    eq(timedelta(hours=1), timedelta(minutes=60))
+    eq(timedelta(minutes=1), timedelta(seconds=60))
+    eq(timedelta(seconds=1), timedelta(milliseconds=1000))
+    eq(timedelta(milliseconds=1), timedelta(microseconds=1000))
+
+  test "float init":
+    template eq(a, b) = check a == b
+    # from CPython/tests/datetimetester.py
+    eq(timedelta(weeks=1.0/7), timedelta(days=1))
+    eq(timedelta(days=1.0/24), timedelta(hours=1))
+    eq(timedelta(hours=1.0/60), timedelta(minutes=1))
+    eq(timedelta(minutes=1.0/60), timedelta(seconds=1))
+    eq(timedelta(seconds=0.001), timedelta(milliseconds=1))
+    eq(timedelta(milliseconds=0.001), timedelta(microseconds=1))
 
   test "normalize":
     proc tG(delta: timedelta, days, seconds, microseconds: int64) =
@@ -108,10 +117,10 @@ suite "TestTimedelta":
     
 
 import std/macros
-suite "TestDate":
+suite "date":
   template theclass: untyped = PyDatetime
   template theclass(x: untyped, xs: varargs[untyped]): untyped =
-    datetime(x, xs)
+    init.datetime(x, xs)
   test "fromisocalendar":
     def test_fromisocalendar():
         # For each test case, assert that fromisocalendar is the
@@ -272,3 +281,23 @@ suite "TestDate":
         # A naive object replaces %z, %:z and %Z w/ empty strings.
         assertEqual(t.strftime("'%z' '%:z' '%Z'"), "'' '' ''")
     test_strftime()
+  test "ctime":
+    let t = theclass(2002, 3, 2)
+    assertEqual(t.ctime(), "Sat Mar  2 00:00:00 2002")
+
+importPyLib time
+suite "tzinfo":
+  test "fromtimestamp":
+    template meth(xs: varargs[untyped]): datetime =
+      datetime.PyDatetime.fromtimestamp(xs)
+    def test_tzinfo_fromtimestamp():
+      ts = time.time()
+      # Ensure it doesn't require tzinfo (i.e., that this doesn't blow up).
+      base = meth(ts)
+      # Try with and without naming the keyword.
+      off42 = newFixedOffset(42, "42")
+      another = meth(ts, off42)
+      again = meth(ts, tz=off42)
+      #assertIs(another.tzinfo, again.tzinfo)
+      assertEqual(another.utcoffset(), timedelta(minutes=42))
+    test_tzinfo_fromtimestamp()
