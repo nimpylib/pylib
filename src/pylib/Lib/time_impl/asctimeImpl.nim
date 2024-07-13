48dc7fb4845a0ac86be50e0a17a7c202818fa305
litlighilit <litlighilit@foxmail.com>
2024-07-13 11:33:19 +0800

    refine: better impl for asctimeImpl

--- src/pylib/Lib/time_impl/asctimeImpl.nim
+++ src/pylib/Lib/time_impl/asctimeImpl.nim
@@ -4,9 +4,12 @@ import std/times
 
 template asctimeImpl*(result: string, dt: DateTime) =
   result.add dt.format "ddd MMM "
-  var day = dt.format "dd "
-  if day[0] == '0': day[0] = ' '
-  result.add day
+  # %2d
+  let day = dt.monthday.int
+  if day < 10:
+    result.add ' ' 
+  result.add $day
+  result.add ' '
   result.add dt.format "HH:mm:ss uuuu"
 
 func asctime*(dt: DateTime): string =
