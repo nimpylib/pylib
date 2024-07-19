90426bf4ea7fbf1378c0a95b66b6f169c6d5ccea
litlighilit <litlighilit@foxmail.com>
2024-07-19 08:36:54 +0800

    func(js): int.{from,to}_bytes, fixes #23

--- src/pylib/numTypes.nim
+++ src/pylib/numTypes.nim
@@ -7,7 +7,7 @@ import ./pyerrors/rterr
 import ./pystring/strimpl
 import ./pybytes/bytesimpl
 
-import ./private/backendMark
+const weridTarget = defined(js) or defined(nimscript)
 
 
 type NimInt* = system.int  ## alias of system.int
@@ -105,22 +105,6 @@ func parseByteOrder(byteorder: string): Endianness =
   else: raise newException(ValueError, "byteorder must be either 'little' or 'big'")
 
 const BigEndian = cpuEndian == bigEndian
-proc transInOrder(outBuf: var openArray[char], inBuf: openArray[char], endianness: Endianness,
-    length, totLen: int) =
-  template asgnWithI(inp, outp){.dirty.} =
-    for i in 0..<length:
-      outBuf[outp] = inBuf[inp]
-  let hi_len = length - 1
-  template handleTargetOrder(inp){.dirty.} =
-    if endianness == bigEndian:
-      asgnWithI(inp, hi_len - i)  # write from higher order.
-    else:
-      asgnWithI(inp, i)
-  when BigEndian:
-    let hi_tot = totLen - 1
-    handleTargetOrder hi_tot - i  # read from higher order.
-  else:
-    handleTargetOrder i
 
 template highByte(b: PyBytes, endian: Endianness, hi = b.len-1): uint8 =
   uint8:
@@ -130,33 +114,82 @@ template highByte(b: PyBytes, endian: Endianness, hi = b.len-1): uint8 =
 template signbitSet(b: uint8): bool =
   (b and 0b1000_0000'u8) == 0b1000_0000'u8
 
-func lowestN_set0_and_rest_setP1[I](totByteLen: static[int], nByte: int): I =
-  ## let the byte containing the sign bit is the highest.
-  ## 0-fill the lowset `nByte` byte and
-  ## 1-fill(all bit as 1) the rest (usually including sign bit).
-  assert sizeof(I) == totByteLen
-  block:
-    var arr: array[totByteLen, int8]  # init with 0
-    template P1range: untyped =
-      when BigEndian: 0..<(totByteLen-nByte)
-      else: nByte..<totByteLen
-    for i in P1range:
-      arr[i] = -1
-    cast[I](arr)
+const arrNotCvtableInt = weridTarget
 
-proc from_bytes(res: var NimInt, bytes: PyBytes, byteorder: Endianness, signed=false) =
-  if not res.fitLen bytes.len:
+when arrNotCvtableInt:
+  template charPart(i: NimInt): char =
+    when compileOption("jsBigInt64"):
+      # NIM-BUG: When char is unsigned, if only `cast[char](i)`
+      # you'll get surprising a char with negative `ord` result!
+      # (and converting it to int results in a negative int)
+      char cast[uint8](i)
+    else:
+      cast[char](i)
+  template loopRangeWithIt(bHi: int, byteorder: Endianness, body: untyped){.dirty.} =
+    if byteorder == bigEndian:
+      for it{.inject.} in countdown(bHi, 0): body
+    else:
+      for it{.inject.} in 0 .. bHi: body
+else:
+  proc transInOrder(outBuf: var openArray[char], inBuf: openArray[char], endianness: Endianness,
+      length, totLen: int) =
+    template asgnWithI(inp, outp){.dirty.} =
+      for i in 0..<length:
+        outBuf[outp] = inBuf[inp]
+    let hi_len = length - 1
+    template handleTargetOrder(inp){.dirty.} =
+      if endianness == bigEndian:
+        asgnWithI(inp, hi_len - i)  # write from higher order.
+      else:
+        asgnWithI(inp, i)
+    when BigEndian:
+      let hi_tot = totLen - 1
+      handleTargetOrder hi_tot - i  # read from higher order.
+    else:
+      handleTargetOrder i
+
+  func lowestN_set0_and_rest_setP1[I](totByteLen: static[int], nByte: int): I =
+    ## let the byte containing the sign bit is the highest.
+    ## 0-fill the lowset `nByte` byte and
+    ## 1-fill(all bit as 1) the rest (usually including sign bit).
+    assert sizeof(I) == totByteLen
+    block:
+      var arr: array[totByteLen, int8]  # init with 0
+      template P1range: untyped =
+        when BigEndian: 0..<(totByteLen-nByte)
+        else: nByte..<totByteLen
+      for i in P1range:
+        arr[i] = -1
+      cast[I](arr)
+
+func add_from_bytes*(res: var NimInt, bytes: PyBytes, byteorder: Endianness, signed=false) =
+  ## EXT.
+  let bLen = bytes.len
+  if bLen == 0:
+    return
+  if not res.fitLen bLen:
     raise newException(OverflowDefect, "Currently NimInt cannot hold so many bytes")
+  let bHi = bytes.len - 1
+  when arrNotCvtableInt:
+    template loopWithIt(expr) =
+      for it{.inject.} in 0..bHi:
+        res += bytes[expr] shl (it*8)
+    if byteorder == littleEndian:
+      loopWithIt it
+    else:
+      loopWithIt bHi - it
+    if signed and bytes.highByte(byteorder, bHi).signbitSet():
+      # we've check bytes is not empty above
+      res -= 1 shl (8 * bLen)
+  else:
+    const totByteLen = getSize(res)
 
-  const totByteLen = getSize(res)
-
-  var holder: array[totByteLen, char]  # Nim init things with zero by default
-  let bytesArr = @bytes
-  transInOrder holder, bytesArr, byteorder, length=bytes.len, totLen=totByteLen
-  res = cast[NimInt](holder)
-  if signed:
-    let bLen = bytes.len
-    if bLen > 0 and bytes.highByte(byteorder, blen-1).signbitSet():
+    var holder: array[totByteLen, char]  # Nim init things with zero by default
+    let bytesArr = @bytes
+    transInOrder holder, bytesArr, byteorder, length=bytes.len, totLen=totByteLen
+    res = cast[NimInt](holder)
+    if signed and bytes.highByte(byteorder, bHi).signbitSet():
+      # we've check bytes is not empty above
       # res -= 1 shl (8 * bLen) <- the same as following line
       res = res or lowestN_set0_and_rest_setP1[NimInt](totByteLen, bLen)
 
@@ -166,14 +199,12 @@ proc from_bytes(res: var NimInt, bytes: PyBytes, byteorder: Endianness, signed=f
     raise newException(OverflowDefect, 
       "signed=false now, but this value has caused overflow")
 
-proc from_bytes*(_: typedesc[NimInt], bytes: PyBytes, byteorder: PyStr, signed=false): NimInt
-    {.noWeirdBackend.} =
+func from_bytes*(_: typedesc[NimInt], bytes: PyBytes, byteorder: PyStr, signed=false): NimInt =
   let endianness = parseByteOrder $byteorder
   result = newInt()
-  result.from_bytes(bytes, endianness, signed)
+  result.add_from_bytes(bytes, endianness, signed)
 
-proc to_bytes*(self; length: int, byteorder: PyStr, signed=false): PyBytes
-    {.noWeirdBackend.} =
+func to_bytes*(self; length: int, byteorder: PyStr, signed=false): PyBytes =
   if length < 0:
     raise newException(ValueError, "length argument must be non-negative")
   if not signed and self < 0:
@@ -185,7 +216,15 @@ proc to_bytes*(self; length: int, byteorder: PyStr, signed=false): PyBytes
   const totByteLen = getSize(self)
   if byteLen > length:
     raise newException(OverflowDefect, "int too big to convert")
-  let cstr = cast[array[totByteLen, char]](self)
-  var s = newString(length)
-  transInOrder s, cstr, endianness, length = length, totLen = totByteLen
+  var s: seq[char]
+
+  when arrNotCvtableInt:
+    s = newSeqOfCap[char](length)
+    loopRangeWithIt(length - 1, endianness):
+      s.add (self shr (it*8)).charPart
+  else:
+    s = newSeq[char](length)
+    let cstr = cast[array[totByteLen, char]](self)
+    transInOrder s, cstr, endianness, length = length, totLen = totByteLen
+
   bytes s
