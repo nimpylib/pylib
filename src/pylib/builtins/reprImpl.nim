991c974480da66f7878fd6e26dec71e26fda6006
litlighilit <litlighilit@foxmail.com>
2024-07-20 21:52:18 +0800

    fix(py): repr
    - uses lower hex alpha instead of upper and repr(bytes)
    - repr(bytes) works for char greater than '\127'

--- src/pylib/builtins/reprImpl.nim
+++ src/pylib/builtins/reprImpl.nim
@@ -1,41 +1,74 @@
 
-proc raw_repr(us: string
-  ,e1: static[bool] = false # if skip(not escape) `'`(single quotation mark)
-  ,e2: static[bool] = false # if skip(not escape) `"`(double quotation mark)
-): string =
-  template add12(s: string, c: char) =
-    when e1:
-      if c == '\'':
-        result.add '\''
-        continue
-    when e2:
-      if c == '"':
-        result.add '"'
-        continue
-    result.addEscapedChar c
-  for c in us:
-    if c > '\127':
-      result.add c  # add non-ASCII utf-8 AS-IS
-    else:
-      when defined(useNimCharEsc): result.add12 c
-      else:
-        if c == '\e': result.add "\\x1b"
-        else: result.add12 c
+const
+  q = '\''
+  Q = '"'
+  Qq = {Q, q}
 
-template implWith(a; rawImpl): untyped =
+
+func Py_addEscapedChar(result: var string, c: char,
+    escapeQuotationMark: static[set[char]] = Qq) =
+  ## snippet from CPython-3.14.0-alpha/Objects/bytesobject.c
+  ## PyBytes_Repr
+  template slash(c: char) =
+    result.add '\\'
+    result.add c
+  template push(c: char) =
+    result.add c
+  case c
+  of '\\': slash '\\'
+  of '\t': slash 't'
+  of '\n': slash 'n'
+  of '\r': slash 'r'
+  of escapeQuotationMark:
+    slash c
+  elif c < ' ' or c.ord > 0x7f:
+    const hexdigits = "0123456789abcdef"
+    slash 'x'
+    let ci = c.ord
+    push hexdigits[(ci and 0xf0) shr 4]
+    push hexdigits[(ci and 0xf)]
+  else:
+    when defined(useNimCharEsc):
+      if c == '\e': result.add "\\x1b"
+      else: push c
+    else: push c
+
+func raw_repr(us: string
+  ,escapeQuotationMark: static[set[char]] = Qq
+  ,escape127: static[bool] = false # if to escape char greater than `\127`
+): string =
+  template addMayEscape(s: string, c: char) =
+    result.Py_addEscapedChar c, escapeQuotationMark
+  for c in us:
+    template addEscaped =
+      result.addMayEscape c
+    when escape127:
+      addEscaped
+    else:
+      if c > '\127':
+        result.add c  # add non-ASCII utf-8 AS-IS
+      else:
+        addEscaped
+
+func mycontains(s: string, c: char): bool =
+  # NIM-BUG: once using `c in s` when `nimble testC/JS`, it hangs forever
+  for i in s:
+    if i == c: return true
+
+template implWith(a; rawImpl; arg_escape127: bool): untyped =
   let us = a  # if a is an expr, avoid `a` being evaluated multiply times 
   when defined(singQuotedStr):
-    '\'' & rawImpl(us) & '\''
+    q & rawImpl(us, escape127=arg_escape127) & q
   else:
-    if '"' in us:
-      '\'' & rawImpl(us, e2 = true) & '\''
+    if us.mycontains Q:
+      q & rawImpl(us, escapeQuotationMark={q}, escape127=arg_escape127) & q
     else:
-      if '\'' in us:
-        '"' & rawImpl(us, e1 = true) & '"'
+      if us.mycontains q:
+        Q & rawImpl(us, escapeQuotationMark={Q}, escape127=arg_escape127) & Q
       else: # neither ' nor "
-        '\'' & rawImpl(us) & '\''
+        q & rawImpl(us, escape127=arg_escape127) & q
 
-func pyreprImpl*(s: string): string =
+func pyreprImpl*(s: string, escape127: static[bool] = false): string =
   ## Python's `repr`
   ## but returns Nim's string.
   ##
@@ -47,7 +80,7 @@ func pyreprImpl*(s: string): string =
     assert repr("\"") == "\"\\\"\""   # string literal of "\""
     # use pyrepr for any StringLike and returns a PyStr
     assert pyreprImpl("\"") == "'\"'"
-  implWith(s, raw_repr)
+  implWith(s, raw_repr, escape127)
 
 func pyreprbImpl*(s: string): string =
-  'b' & s.pyreprImpl
+  'b' & s.pyreprImpl(true)
--- tests/tbytes.nim
+++ tests/tbytes.nim
@@ -1,17 +1,24 @@
 
-test "bytes":
-  def t_getitem():
-    byt = b"asd"
-    assert byt[1] == ord('s')
-  
-  t_getitem()
+suite "bytes":
+  test "getitem":
+    def t_getitem():
+      byt = b"asd"
+      assert byt[1] == ord('s')
+    
+    t_getitem()
 
-test "bytes meth":
-  def t_splitlines():
-    ls = b"asd\r\nfdfg".splitlines()
-    assert ls == [b"asd", b"fdfg"]
-  t_splitlines()
+  test "meth":
+    def t_splitlines():
+      ls = b"asd\r\nfdfg".splitlines()
+      assert ls == [b"asd", b"fdfg"]
+    t_splitlines()
 
-  let btab = PyBytes.maketrans(b"bc", b"23")
+    let btab = PyBytes.maketrans(b"bc", b"23")
 
-  check b"HbOa".translate(btab, b"a") == b"H2O"
+    check b"HbOa".translate(btab, b"a") == b"H2O"
+
+  test "repr":
+    check repr(b"\xfe\xff") == "b'\\xfe\\xff'"
+    #check repr(b"\xfe\"\xff") == "b'\\xfe\"\\xff'"
+    # XXX: b"\xfe\"\xff" in Nim is parsed as `b"\xfe\"` `\xff"` instead of one string lit.
+    check repr(b"\xfe'\xff") ==  "b\"\\xfe'\\xff\""
