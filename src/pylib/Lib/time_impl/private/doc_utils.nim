66d358b92418810420567a1eb0b9f761cf315cdb
litlighilit <litlighilit@foxmail.com>
2024-07-21 11:02:24 +0800

    chore: faster impl for time_impl/doc_utils when not nimdoc, also fix warning

--- src/pylib/Lib/time_impl/private/doc_utils.nim
+++ src/pylib/Lib/time_impl/private/doc_utils.nim
@@ -1,34 +1,29 @@
 
 import std/tables
-import std/macros
-
-func addDocOfImpl(docFileName: string): NimNode =
-  newCommentStmtNode(
-      docFileName
-  )
-
-func addDocOfImpl(docFileName: string; def: NimNode): NimNode =
-  result = def
-  result.body.insert(0, addDocOfImpl docFileName)
-
 type
   DocTable* = object
     module: string
     api: Table[string, string]
 
-func initDocTable*(moduleDoc: string, init: openArray[(string, string)]): DocTable{.compileTime.} =
-  ## .. hint:: key `""` stands for the module level doc.
-  ## 
-  DocTable(
-    module: moduleDoc,
-    api: init.toTable
-  )
-
 when defined(nimdoc):
+  import std/macros
+
+  func initDocTable*(moduleDoc: string, init: openArray[(string, string)]): DocTable{.compileTime.} =
+    ## .. hint:: key `""` stands for the module level doc.
+    ## 
+    DocTable(
+      module: moduleDoc,
+      api: init.toTable
+    )
+
+
+  func addDocImpl(doc: string; def: NimNode): NimNode =
+    result = def
+    result.body.insert(0, newCommentStmtNode doc)
   macro fetchDoc*(tab: static DocTable) =
     ## used for fetch module level doc
     let doc = tab.module
-    result = addDocOfImpl doc
+    result = addDocImpl doc
 
   macro fetchDoc*(tab: static DocTable; def) =
     ## used as proc's pragma to fetch doc
@@ -36,7 +31,9 @@ when defined(nimdoc):
     let fn = tab.api.getOrDefault funcName
     if fn.len == 0:
       return def
-    result = addDocOfImpl(fn, def)
+    result = addDocImpl(fn, def)
 else:
+  func initDocTable*(moduleDoc: string, init: openArray[(string, string)]): DocTable{.compileTime.} =
+    discard
   template fetchDoc*(_: static DocTable) = discard
   template fetchDoc*(_: static DocTable; def) = def
