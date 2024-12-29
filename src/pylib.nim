eef3cb9e71ae4435b471839e1085ebd8cb338b42
litlighilit <litlighilit@foxmail.com>
2024-12-30 01:48:09 +0800

    doc(pylib.nim): add version, warn for `lenientops`

--- src/pylib.nim
+++ src/pylib.nim
@@ -1,8 +1,25 @@
-## .. include:: ../doc/pylib.md
+
+const WarnLeniOps = "`std/lenientops` was imported automatically. " &
+  "Compile with -d:pylibNoLenient to disable it " &
+  "if you wish to do int->float conversions yourself"
+
+when not defined(pylibNoLenient):
+  {.warning: WarnLeniOps.}
+  import std/lenientops
+  export lenientops
+
+when defined(nimdoc):
+  from pylib/version import Version
+  import std/macros
+  macro doc(s: static[string]): untyped = newCommentStmtNode s
+
+  doc "> Welcome to pylib " & Version
+  ## .. include:: ../doc/pylib.md
+  doc ".. warning:: " & WarnLeniOps
 
 import pylib/Lib/timeit
 
-when not defined(js):
+when not defined(js) and not defined(nimscript):
   import pylib/io
   export io
 import pylib/private/trans_imp
@@ -13,10 +30,6 @@ impExp pylib,
   pystring, pybytes, pybytearray,
   pysugar
 
-when not defined(pylibNoLenient):
-  {.warning: "'lenientops' module was imported automatically. Compile with -d:pylibNoLenient to disable it if you wish to do int->float conversions yourself".}
-  import std/lenientops
-  export lenientops
 
 template timeit*(repetitions: int, statements: untyped): untyped{.deprecated:
     "will be removed from main pylib since 0.10, import it from `pylib/Lib` instead".} =
