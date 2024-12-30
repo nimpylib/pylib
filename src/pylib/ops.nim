621cb028890408108eb449f9b629d944ad41e70d
litlighilit <litlighilit@foxmail.com>
2024-12-30 15:22:09 +0800

    doc(ops): amend comment doc

--- src/pylib/ops.nim
+++ src/pylib/ops.nim
@@ -2,9 +2,18 @@
 import std/math
 from ./pyerrors/aritherr import ZeroDivisionError
 
+## ## why no `/=`?
+## For `template `/=`*(x: var SomeInteger, y: SomeInteger)`:
+## 
+## Nim is static-typed, but `/=` will cause lhs convert from int to float
+
 
 # Power templates for different types of arguments
 template `**`*[T](a: T, b: Natural): T =
+  ## power for `b` is a non-static integer:
+  ## 
+  ## only Natural is acceptable unless `b` is static,
+  ## see `\`**\`(a: T; b: static[int])`_
   runnableExamples:
     var i = -1
     doAssertRaises RangeDefect:
@@ -14,7 +23,15 @@ template `**`*[T](a: T, b: Natural): T =
   bind `^`
   a ^ b
 
-template `**`*[T: SomeNumber](a: T; b: static[int]): int|float =
+template `**`*[T: SomeInteger](a: T; b: static[int]): int|float =
+  ## power for static int `b`
+  ## 
+  ## so that result type can be inferred:
+  ## 
+  ## - int if `b >= 0`
+  ## - float if `b < 0`
+  ## 
+  ## For non-static int `b`, `\`**\`(a: T; b: Natural)`_ is used.
   runnableExamples:
     const f = 5 ** -1  # only when the rhs is static[int]
     assert f == 0.2
@@ -25,17 +42,28 @@ template `**`*[T: SomeNumber](a: T; b: static[int]): int|float =
     pow(a.float, b.float)  # returns float
   else:
     bind `^`
-    a ^ b  # returns int or floatd
+    a ^ b  # returns int
 
-template `**`*[T: SomeFloat](a, b: T): T = bind pow; pow(a, b)
-template `**`*[A: SomeFloat, B: SomeInteger](a: A, b: B): A = bind pow; pow(a, A(b))
-template `**`*[A: SomeInteger; B: SomeFloat](a: A, b: B): B = bind pow; pow(B(a), b)
+template `**`*[T: SomeFloat](a, b: T): T =
+  bind pow
+  pow(a, b)
+template `**`*[A: SomeFloat, B: SomeInteger](a: A, b: B): A =
+  bind pow
+  pow(a, A(b))
+template `**`*[A: SomeInteger; B: SomeFloat](a: A, b: B): B =
+  bind pow
+  pow(B(a), b)
 
-template `**=`*(a: var SomeNumber, b: SomeNumber) = a = a**b
+template `**=`*(a: var SomeNumber, b: SomeNumber) =
+  bind `**`
+  a = a**b
 
 # Currently `shr` is also `arithm shr`, but it used to be `logic shr`
 template `>>`*[I: SomeInteger](a, b: I): I = ashr a, b
-template `<<`*[I: SomeInteger](a, b: I): I = a shl b
+template `<<`*[I: SomeInteger](a, b: I): I =
+  ## .. warning:: this causes overflow silently.
+  ##  Yet Python's int never overflows.
+  a shl b
 
 template `>>=`*[I: SomeInteger](a, b: I) = a = a shr b
 template `<<=`*[I: SomeInteger](a, b: I) = a = a shl b
@@ -56,21 +84,20 @@ template `<>`*[A: SomeFloat, B: SomeInteger](a: A, b: B): bool = a != A(b) # Pyt
 
 template `/`*(x: SomeInteger, y: SomeInteger): float = system.`/`(float(x), float(y))
 
-# Nim is static-typed, but `/=` will cause lhs convert from int to float
-# template `/=`*(x: var SomeInteger, y: SomeInteger)  
-
 
 template zeRaise(x) =
   if x == typeof(x)(0):
     raise newException(ZeroDivisionError, "division or modulo by zero")
 
 func `%`*[T: SomeNumber](a, b: T): T =
-  ## Python-like modulo
+  ## Python's modulo (a.k.a `floor modulo`)  (`quotient`)
+  ## 
+  ## .. hint:: Nim's `mod` is the same as `a - b * (a div b)` (`remainder`),
+  ##   just like `C`.
   runnableExamples:
     assert 13 % -3 == -2
     assert -13 % 3 == 2
   zeRaise b
-  # Nim's `mod` is the same as `a - b * (a // b)` (i.e. remainder), while Py's is not.
   floorMod a,b
 
 template `%`*[A: SomeFloat, B: SomeInteger](a: A, b: B): A = a % A(b)
@@ -79,7 +106,10 @@ template `%`*[A: SomeInteger; B: SomeFloat](a: A, b: B): B = B(a) % b
 template `%=`*(self: var SomeNumber, x: SomeNumber) = self = self % x
 
 func `//`*[A, B: SomeFloat | SomeInteger](a: A, b: B): SomeNumber {.inline.} =
-  ## Python-like floor division
+  ## Python's division (a.k.a. `floor division`)
+  ## 
+  ## .. hint:: Nim's `div` is division with any fractional part discarded
+  ##    (a.k.a."truncation toward zero"), just like `C`.
   runnableExamples:
     assert 13 // -3 == -5
     assert 13 div -3 == -4
@@ -92,7 +122,7 @@ template `//=`*[A, B: SomeFloat | SomeInteger](a: var A, b: B)=
   a = a//b
 
 func divmod*[T: SomeNumber](x, y: T): (T, T) = 
-  ## differs from std/math divmod
+  ## differs from `std/math` `divmod`, see `//`_ and `%`_ for details.
   (x//y, x%y)
 
 template `==`*(a, b: typedesc): bool =
