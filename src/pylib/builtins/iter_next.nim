568a8c398105134fab2cbdb96e450bb1aa871040
litlighilit <litlighilit@foxmail.com>
2025-01-01 05:15:54 +0800

    fixup: EXT: nextImpl

--- src/pylib/builtins/iter_next.nim
+++ src/pylib/builtins/iter_next.nim
@@ -1,6 +1,7 @@
 ## builtins.iter
 
 import ../collections_abc
+import std/options
 
 #iterator items*[T](it: PyIterable[T]): T = for i in it.iter: yield i
 
@@ -35,8 +36,17 @@ iterator items*[T](self: PyIterator[T]): T =
   for i in self.iter():
     yield i
 
+proc nextImpl*[T](self: PyIterator[T]): Option[T]{.inline.} =
+  ## EXT. Get rid of exception for faster iteration.
+  result = some self.iter()
+  if self.iter.finished(): result = none(T)
+
+proc nextImpl*[T](self: PyIterator[T], res: var T): bool{.inline.} =
+  ## EXT. Get rid of exception for faster iteration.
+  res = self.iter()
+  not self.iter.finished()
+
 proc next*[T](self: PyIterator[T]): T =
   # see manual, sysmte.finished shall validate after a value is getten from iterator.
-  result = self.iter()
-  if self.iter.finished():
+  if not self.nextImpl result:
     raise newException(StopIteration, "iterator stop")
