676f407c53e3546c779e8ca0499305e564ccc956
litlighilit <litlighilit@foxmail.com>
2025-02-01 14:10:00 +0800

    impr(pytuple): support items(()) [skip ci]

--- src/pylib/builtins/pytuple.nim
+++ src/pylib/builtins/pytuple.nim
@@ -8,13 +8,21 @@ macro items*(t: tuple): untyped =
     for i in t:
       result.add i
   else:
-    for i in 0..<t.getTypeImpl.len:
+    let nElem = t.getTypeImpl.len
+    if nElem == 0:
+      return parseExpr(
+        "items(array[0, int]([]))"
+      ) # any type shall be ok
+    for i in 0..<nElem:
       result.add nnkBracketExpr.newTree(t, newLit i)
   result = newCall(ident"items", result)
 
 when isMainModule:
   # not compile:
   #for i in (1, 2.0): echo i
+
+  for i in (): echo i
+
   for i in (1, 2): echo i
   let t = (1, 2)
   for i in t:
