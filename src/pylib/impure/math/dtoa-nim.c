ced7ee8e518e14a92ab186141710372cc47fe116
litlighilit <litlighilit@foxmail.com>
2025-02-17 05:31:19 +0800

    impr(builtins): round supports threads

---
+++ src/pylib/impure/math/dtoa-nim.c
@@ -0,0 +1,8 @@
+
+void ACQUIRE_DTOA_LOCK(int);
+void FREE_DTOA_LOCK(int);
+
+int dtoa_get_threadno(void);
+
+#include "./dtoa.c"
+
--- src/pylib/impure/math/dtoa.nim
+++ src/pylib/impure/math/dtoa.nim
@@ -4,21 +4,43 @@
 
 import ../../builtins/private/pycore_pymath
 
+const threads = compileOption("threads")
+when threads:
+  import std/locks
+  var dtoa_locks: array[2, Lock]
+  for l in dtoa_locks.mitems:
+    l.initLock
 
+  proc ACQUIRE_DTOA_LOCK*(n: c_int){.exportc.} = dtoa_locks[n].acquire()
+  proc FREE_DTOA_LOCK*(n: c_int){.exportc.} = dtoa_locks[n].release()
+  const threadnoInC = "dtoa_get_threadno"
+  proc getThreadId*: c_int{.exportc: threadnoInC.} = c_int system.getThreadId()
 
 const Cflags = block:
   var cflags = ""
+  const Dpre = # TODO: use `#define` to C code, maybe there's compiler rejects /D,-D
+    when defined(vcc): " /D"
+    else: " -D"
+
+  template addD(sym) =
+      cflags.add Dpre
+      cflags.add astToStr sym
   template define(sym, cond) =
     when cond:
-      cflags.add " -D"
-      cflags.add astToStr sym
+      addD sym
 
-  #[ TODO: supports threads:on
-  const threads = compileOption("threads")
+  template defineVal(sym, val) =
+    addD sym
+    cflags.add "="
+    cflags.add val
+  template defineVal(sym) = defineVal(sym, astToStr sym)
+  
   define MULTIPLE_THREADS, threads
   when threads:
     # need to define ACQUIRE_DTOA_LOCK(n) and FREE_DTOA_LOCK(n) where n is 0 or 1
-  ]#
+    defineVal ACQUIRE_DTOA_LOCK
+    defineVal FREE_DTOA_LOCK
+    defineVal dtoa_get_threadno, threadnoInC
 
   #[ This code should also work for ARM mixed-endian format on little-endian
     machines, where doubles have byte order 45670123 (in increasing address
@@ -27,7 +49,7 @@ const Cflags = block:
   define IEEE_MC68k,  DOUBLE_IS_BIG_ENDIAN_IEEE754 or DOUBLE_IS_ARM_MIXED_ENDIAN_IEEE754
   cflags
 
-{.compile("dtoa.c", Cflags).}
+{.compile("dtoa-nim.c", Cflags).}
 
 proc dtoa_r*(
     dd: cdouble,
