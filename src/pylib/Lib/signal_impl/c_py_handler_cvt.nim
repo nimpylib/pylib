902e1c29c539e953918c16fcfc080700e62f5186
litlighilit <litlighilit@foxmail.com>
2025-03-26 17:30:09 +0800

    feat(Lib): signal.signal accept NimSigHandler

--- src/pylib/Lib/signal_impl/c_py_handler_cvt.nim
+++ src/pylib/Lib/signal_impl/c_py_handler_cvt.nim
@@ -1,12 +1,12 @@
 
 import ./pylifecycle
 
-proc toCSighandler*(p: PySighandler): CSigHandler =
+proc toCSighandler*(p: PySigHandler): CSigHandler =
   proc (signalnum: cint){.noconv.} =
     let frame = getFrame() #  this closure's
     p(signalnum, frame.prev.prev)
 
 
-proc toPySighandler*(p: CSighandler): PySigHandler =
+proc toPySighandler*(p: CSigHandler|NimSigHandler): PySigHandler =
   proc (signalnum: int, _: PFrame){.closure.} =
     p(cint signalnum)
