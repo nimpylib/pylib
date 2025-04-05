1d5e2a8e3482a04f5e3d2dea3084aa57393f08bf
litlighilit <litlighilit@foxmail.com>
2025-04-05 15:32:27 +0800

    refact: mv os_impl/private/platform_util root's private

---
+++ src/pylib/private/platform_utils.nim
@@ -0,0 +1,42 @@
+
+import std/macros
+
+const nimDoc = defined(nimdoc)
+macro platformAvailImpl(inPlat: static[bool]; platStr: static[string]; def) =
+  if nimDoc:
+    def.body = newStmtList(
+      newCommentStmtNode ".. hint:: `Availability" &
+        "<https://docs.python.org/3/library/intro.html#availability>`_: " &
+          platStr,
+      nnkDiscardStmt.newTree newEmptyNode()
+    )
+    return def
+  if inPlat:
+    return def
+  result = def
+  result.body = newEmptyNode()
+  result.addPragma newColonExpr(
+    ident"error",
+    newLit "this is only available on platform: " & platStr & '.'
+  )
+
+template platformAvail*(platform; def) =
+  ## Pragma on procs to generate doc of sth like `Availability: Windows.`
+  ##
+  ## Currently, `platform` must be something
+  ## that can be put within `defined`.
+  bind platformAvailImpl
+  platformAvailImpl(defined(platform), astToStr(platform), def)
+
+template platformAvailWhen*(platform; cond: bool; def) =
+  bind platformAvailImpl
+  platformAvailImpl(defined(platform) and cond,
+    astToStr(platform) & " when " & astToStr(cond), def)
+
+template platformUnavail*(platform; def) =
+  bind platformAvailImpl
+  platformAvailImpl(not defined(platform), "not " & astToStr(platform), def)
+
+template platformNoJs*(def) =
+  bind platformUnavail
+  platformUnavail(js, def)
