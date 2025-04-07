ae1671e68a37be3f707406744753756c71c25c1a
litlighilit <litlighilit@foxmail.com>
2025-04-07 15:45:34 +0800

    feat(js): jsutils/consts from_js_const

---
+++ src/pylib/jsutils/consts.nim
@@ -0,0 +1,13 @@
+
+const
+  InNodeJs = defined(nodejs)
+import std/jsffi
+when InNodeJs:
+  let econsts = require("constants")
+else:
+  let econsts{.importjs: """(await import("node:constants")).errno""".}: JsObject
+
+template from_js_const*(name; defval: int): int =
+  bind econsts, isUndefined, to, `[]`
+  let n = econsts[astToStr(name)]
+  if n.isUndefined: defVal else: n.to(int)
