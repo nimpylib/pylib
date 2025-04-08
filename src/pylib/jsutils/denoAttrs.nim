8f22a68c8ab3bcbb32f1c9a6b932b9ab70311e5b
litlighilit <litlighilit@foxmail.com>
2025-04-08 18:56:56 +0800

    feat(js): importDenoOrNodeMod;2-arg importDenoOrProcess

--- src/pylib/jsutils/denoAttrs.nim
+++ src/pylib/jsutils/denoAttrs.nim
@@ -42,29 +42,37 @@ when defined(js):
   macro importByNodeOrDeno*(node, deno: static[string]; def) =
     ## pragma
     importByNodeOrDenoImpl(def, nodeExpr=node, denoExpr=deno)
-  proc importDenoOrImpl(def: NimNode; objInNode: string; attr: NimNode): NimNode =
-    var
-      sAttr =
-        if attr.kind in {nnkStrLit, nnkRStrLit, nnkTripleStrLit}:
-          # allow using some expr that cannot be written as Nim expr
-          attr.strVal
-        else:
-          # allow nnkCall, ident and more complex expr
-          repr(attr)
+  proc importDenoOrImpl(def: NimNode; objInNode: string; denoAttr, nodeAttr: NimNode): NimNode =
+    let
+      denoAttr = repr denoAttr
+      nodeAttr = repr nodeAttr
     importByNodeOrDenoImpl(def,
-      nodeExpr=objInNode&'.'&sAttr,
-      denoExpr="Deno."&sAttr
+      nodeExpr=objInNode&'.'&nodeAttr,
+      denoExpr="Deno."&denoAttr
     )
   macro importDenoOr*(objInNode, attr; def) =
+    importDenoOrImpl(def, $objInNode, attr, attr)
+
+  func requireExpr(module: string): string = "require('" & module & "')"
+  macro importDenoOrNodeMod*(modInNode, attr; def) =
+    importDenoOrImpl(def, requireExpr $modInNode, attr)
+  macro importDenoOr*(objInNode, attr; def) =
     importDenoOrImpl(def, $objInNode, attr)
-  macro importDenoOrProcess*(attr; def) =
-    importDenoOrImpl(def, "process", attr)
 
   proc importNodeImpl(def: NimNode, module, symExpr: string): NimNode =
     importByNodeOrDenoImpl(def,
-      "require('" & module & "')." & symExpr,
+      requireExpr(module) & '.' & symExpr,
       "(await import('" & module & "'))." & symExpr
     )
+
+  proc importDenoOrProcessAux(denoAttr, nodeAttr, def: NimNode): NimNode =
+    importDenoOrImpl(def, "process", denoAttr, nodeAttr)
+
+  macro importDenoOrProcess*(denoAttr, nodeAttr; def) =
+    importDenoOrProcessAux(denoAttr, nodeAttr, def)
+  macro importDenoOrProcess*(attr; def) =
+    importDenoOrProcessAux(attr, attr, def)
+
   macro importNode*(module, symExpr; def) =
     let ssym = case symExpr.kind
     of nnkStrLit, nnkRStrLit, nnkTripleStrLit, nnkIdent: symExpr.strVal  # also allows dotExpr
