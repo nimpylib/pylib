bff0b72d963e4c6ef7bdc5c039243b38871e6dc7
litlighilit <litlighilit@foxmail.com>
2025-04-09 14:56:53 +0800

    feat(Lib/unittest): skip* now will output [SKIPPED] ... instead of nothing

--- src/pylib/Lib/unittest/case_py.nim
+++ src/pylib/Lib/unittest/case_py.nim
@@ -91,21 +91,39 @@ gen2 assertGreaterEqual, `>=`
 gen2 assertIsInstance, `is`
 gen2 assertNotIsInstance, `is_not`
 
-template addSkip(reason) = checkpoint reason  ##\
-## XXX: TODO: not really python-like
+func fmtSkip(name, reason: string): string = name & ": " & reason
 
 template skipTest*(reason: string){.genSelf.} =
-  bind skip
-  addSkip reason
+  bind skip, fmtSkip
+  const msg = fmtSkip(astToStr(self), reason)
+  when declared(testSuiteName):
+    testSuiteName = msg
+  else:
+    checkpoint msg
+  ## XXX: TODO: not really python-like
   skip()
+
+proc skipTest(testStmt, reason: NimNode): NimNode =
+  expectKind testStmt, nnkCommand
+  result = newNimNode nnkCommand
+  result.add testStmt[0]
+  let name = testStmt[1]
+  result.add newCall(bindSym"fmtSkip", name, reason)
+  result.add newStmtList(
+    newCall(bindSym"skip")
+  )
 
 template skip*(reason; body) =
   ## EXT.
-  addSkip reason
+  bind skipTest
+  skipTest(body, reason)
 
 template skip*(reason: string): untyped =
-  addSkip reason
-  proc (_: proc) = discard
+  bind skip, fmtSkip
+  template temp(p: proc) =
+    test fmtSkip(astToStr(p), reason):
+      skip()
+  temp
 
 proc asis_id[P: proc](x: P): P = x
 
@@ -114,20 +132,37 @@ template skipIf*(condition: bool, reason: string): proc =
   if condition: skip(reason)
   else: asis_id
 
-template doIf(cond: bool; body) =
-  if cond: body
-template doIf(cond: static bool; body) =
-  when cond: body
+macro makeSkip(cond: bool; reason: string; body; staticBanch: static[bool]) =
+  result = newNimNode(if staticBanch: nnkWhenStmt else: nnkIfStmt)
+  result.add nnkElifBranch.newTree(
+    cond, body
+  )
+  var elseBody = newStmtList()
+  if body.kind == nnkStmtList:
+    for s in body:
+      elseBody.add skipTest(s, reason)
+  elif body.kind == nnkCommand and body[0].eqIdent "test":
+    # test "xxx"
+    elseBody.add skipTest(body, reason)
+  result.add nnkElse.newTree elseBody
+
+template doOrSkip(cond: bool; reason: string; body) =
+  bind makeSkip
+  makeSkip cond, reason, body, false
+template doOrSkip(cond: static bool; reason: string; body) =
+  bind makeSkip
+  makeSkip cond, reason, body, true
 
 template skipIf*(condition: bool, reason: string; body) =
-  bind doIf
-  addSkip reason
-  doIf not condition, body
+  ## EXT.
+  bind doOrSkip
+  doOrSkip not condition, reason, body
 
 template skipUnless*(condition: bool, reason: string): proc =
   bind skipIf
   skipIf(not condition, reason)
 
 template skipUnless*(condition: bool, reason: string; body) =
+  ## EXT.
   bind skipIf
   skipIf(not condition, reason, body)
