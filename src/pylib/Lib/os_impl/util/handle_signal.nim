a5bae2c6e3a3913859ddbd1fad8702c10d556b3d
litlighilit <litlighilit@foxmail.com>
2025-04-20 12:42:21 +0800

    fixup: initVal_with_handle_signal deadloop(d9e0a00938e), os.wait*(f25a731fce5)

--- src/pylib/Lib/os_impl/util/handle_signal.nim
+++ src/pylib/Lib/os_impl/util/handle_signal.nim
@@ -10,10 +10,10 @@ template initVal_with_handle_signal*[R](res: var R; resExpr){.dirty.} =
 
   while true:
     res = resExpr
-    if res >= 0 or isErr(EINTR):
+    if res >= 0 or not isErr(EINTR):
       break
     async_err = PyErr_CheckSignals()
-    if async_err != 0:
+    if async_err == 0:
       break
   if res < 0:
     raiseErrno getErrno()
