cd9ed6442a1443931745c9a2cdde2abf57ad0902
litlighilit <litlighilit@foxmail.com>
2025-04-21 06:41:43 +0800

    fixup! fix(list): slice with negative value causes RangeDefect;
    fix setitem discarded longer values

--- src/pylib/mutSeqSliceOp.nim
+++ src/pylib/mutSeqSliceOp.nim
@@ -3,8 +3,6 @@
 
 import ./builtins/pyslice
 
-import std/enumerate
-
 template loopDown(a, b; step) =
   ## .. also reverse to avoid indices changing
   for i in countdown(min(len(ms)-1, a), max(0, b), step):
@@ -25,11 +23,42 @@ template genDelItem*(Arr){.dirty.} =
     else:
       loopDown(b, indices.start, indices.step)
 
+template moveItems(ms, dest, frm, n) =
+  ## .. also reverse to avoid indices changing
+  when declared(moveMem):
+    when compiles(addr ms[dest]):
+      moveMem(addr ms[dest], addr ms[frm], n*sizeof(ms[0]))
+    else:
+      moveMem(ms.getPtr dest, ms.getPtr frm, n*sizeof(ms[0]))
+  else:
+    if frm > dest:
+      for i in 0..<n:
+        ms[dest+i] = ms[frm+i]
+    else:
+      for i in countdown(n-1, 0):
+        ms[dest+i] = ms[frm+i]
 
 template bodySetItem*(Arr){.dirty.} =
-  bind enumerate
-  for i, idx in enumerate(indices.b .. indices.a):
-    ms[idx] = o[i]
+  bind moveItems
+  var ilow = indices.a
+  let ihigh = indices.b + 1
+  let norig = ihigh - ilow
+  assert norig >= 0
+  let n = o.len
+  let d = n - norig
+  let le = ms.len
+  if le + d == 0:
+    ms.clear()
+    return
+  if d < 0:  # Delete -d items
+    moveItems(ms, ihigh+d, ihigh, le-ihigh)
+    ms.setLen le+d
+  elif d > 0:  # Insert d items
+    ms.setLen le+d
+    moveItems(ms, ihigh+d, ihigh, le-ihigh)
+  for k in 0..<n:
+    ms[ilow] = o[k]
+    ilow.inc
 
 template genGenericSetItem*(Arr, Arr2){.dirty.} =
   bind bodySetItem
