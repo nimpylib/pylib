ac0d4593520195343dbb38cabbf17ea3c0b67b82
litlighilit <litlighilit@foxmail.com>
2025-04-24 02:34:16 +0800

    fix(py): OSError([arg]): works

--- src/pylib/pyerrors/oserr/init.nim
+++ src/pylib/pyerrors/oserr/init.nim
@@ -6,6 +6,9 @@ template gen(name, typ, thirdDefVal){.dirty.} =
   proc name*(myerrno: cint, strerr: string,
       filename = thirdDefVal, winerror: cint = 0, filename2 = ""): ref PyOSError =
     OSError_new[types.typ](true, myerrno, strerr, filename, winerror, filename2)
+  proc name*(strerr: string = ""): ref PyOSError =
+    new result
+    result.strerror = strerr
 
 template gen(typ, thirdDefVal){.dirty.} =
   gen(`new typ`, typ, thirdDefVal)
